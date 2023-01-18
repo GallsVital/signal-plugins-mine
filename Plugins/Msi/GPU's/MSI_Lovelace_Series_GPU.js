384748f34e5922cf05616ccd50aac676eb21c093
TheDordo <ripet1634@gmail.com>
2023-01-18 16:36:34 -0500

    LC100 Component, Alienware 15R3 Added, Asrock Mobo Controller 3 Headers, Alloy Origins 65, AL Fix, Monitor Additions, 4070 MSI, Razer LianLi Case, Teamgroup RAM

--- Plugins/Msi/GPU's/MSI_Lovelace_Series_GPU.js
+++ Plugins/Msi/GPU's/MSI_Lovelace_Series_GPU.js
@@ -258,6 +258,7 @@ class NvidiaGPUDeviceIds {
 		this.RTX3080TI       = 0x2208;
 		this.RTX3090         = 0x2204;
 		this.RTX3090TI       = 0x2203;
+		this.RTX4070TI		 = 0x2782;
 		this.RTX4080		 = 0x2704;
 		this.RTX4090		 = 0x2684;
 	}
@@ -267,6 +268,8 @@ const Nvidia = new NvidiaGPUDeviceIds();
 
 class MSIGPUDeviceIDs {
 	constructor() {
+		this.RTX4070TI_GAMING_X_TRIO				 = 0x5132;
+
 		this.RTX4080_SUPRIM							 = 0x5110;
 		this.RTX4080_GAMING_X_TRIO					 = 0x5111;
 
@@ -282,6 +285,7 @@ class MSIGPUList {
 		const MSIGPUIDs  = new MSIGPUDeviceIDs();
 		this.devices =
         [
+        	new MSIGPUIdentifier(Nvidia.RTX4070TI, MSIGPUIDs.RTX4070TI_GAMING_X_TRIO, 0x68, "MSI RTX 4070Ti GAMING X TRIO"),
         	new MSIGPUIdentifier(Nvidia.RTX4080, MSIGPUIDs.RTX4080_SUPRIM, 0x68, "MSI RTX 4080 SUPRIM"),
         	new MSIGPUIdentifier(Nvidia.RTX4080, MSIGPUIDs.RTX4080_GAMING_X_TRIO, 0x68, "MSI RTX 4080 GAMING X TRIO"),
         	new MSIGPUIdentifier(Nvidia.RTX4090, MSIGPUIDs.RTX4090_GAMING_TRIO, 0x68, "MSI RTX 4090 GAMING TRIO"),
