b87b287847730f799fd7ef92c6f8f77809dff50e
Guilherme <guicandido1337@hotmail.com>
2023-02-08 22:48:52 -0300

    New PIDs

--- Plugins/Asus/GPUs/Asus_Ampere_Lovelace_GPU.js
+++ Plugins/Asus/GPUs/Asus_Ampere_Lovelace_GPU.js
@@ -389,6 +389,7 @@ class Asus_Ampere_Lovelace_IDs {
 
 		this.RTX4070TI_TUF_GAMING_OC			   = 0x88A3;
 		this.RTX4080_TUF_GAMING					   = 0x88A1;
+		this.RTX4080_TUF_GAMING_2				   = 0x88A2;
 		this.RTX4080_STRIX_GAMING_OC 			   = 0x88BF;
 		this.RTX4080_TUF_GAMING_OC				   = 0x88CA;
 
@@ -493,6 +494,7 @@ const Asus3000GPUIDs =
 
 	new AsusGPUIdentifier(Nvidia.RTX4070TI, AsusID.RTX4070TI_TUF_GAMING_OC, "Asus TUF RTX 4070Ti Gaming OC"),
 	new AsusGPUIdentifier(Nvidia.RTX4080, AsusID.RTX4080_TUF_GAMING, "Asus TUF RTX 4080 Gaming"),
+	new AsusGPUIdentifier(Nvidia.RTX4080, AsusID.RTX4080_TUF_GAMING_2, "Asus TUF RTX 4080 Gaming"),
 	new AsusGPUIdentifier(Nvidia.RTX4080, AsusID.RTX4080_STRIX_GAMING_OC, "Asus ROG Strix 4080 Gaming OC"),
 	new AsusGPUIdentifier(Nvidia.RTX4080, AsusID.RTX4080_TUF_GAMING_OC, "Asus TUF RTX 4080 Gaming OC"),
 
