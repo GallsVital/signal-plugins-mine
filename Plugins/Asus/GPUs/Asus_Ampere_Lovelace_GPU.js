92ddc5d3dc6c495ebee35eab7fb505443f8ffdb7
Guilherme <guicandido1337@hotmail.com>
2023-02-14 18:04:29 -0300

    4070TI TUF Gaming OC support

--- Plugins/Asus/GPUs/Asus_Ampere_Lovelace_GPU.js
+++ Plugins/Asus/GPUs/Asus_Ampere_Lovelace_GPU.js
@@ -388,6 +388,7 @@ class Asus_Ampere_Lovelace_IDs {
 		this.RTX3090TI_TUF_GAMING				   = 0x8874;
 
 		this.RTX4070TI_TUF_GAMING_OC			   = 0x88A3;
+		this.RTX4070TI_TUF_GAMING_OC_2			   = 0x88A4;
 		this.RTX4080_TUF_GAMING					   = 0x88A1;
 		this.RTX4080_STRIX_GAMING_OC 			   = 0x88BF;
 		this.RTX4080_TUF_GAMING_OC				   = 0x88CA;
@@ -452,7 +453,7 @@ const Asus3000GPUIDs =
 
 	new AsusGPUIdentifier(Nvidia.RTX3080, AsusID.RTX3080_STRIX_GAMING_WHITE, "Asus ROG Strix 3080 O10G White Gaming"),
 	new AsusGPUIdentifier(Nvidia.RTX3080_LHR, AsusID.RGB3080_STRIX_GAMING_V2, "Asus ROG Strix 3080 O10G Gaming LHR"),
-	
+
 	new AsusGPUIdentifier(Nvidia.RTX3080_LHR, AsusID.RTX3080_STRIX_GAMING_WHITE_OC_LHR, "Asus ROG Strix 3080 O10G White OC LHR"),
 	new AsusGPUIdentifier(Nvidia.RTX3080, AsusID.RTX3080_STRIX_GAMING_GUNDAM, "Asus ROG Strix 3080 O10G Gundam"),
 	new AsusGPUIdentifier(Nvidia.RTX3080, AsusID.RTX3080_STRIX_GAMING, "Asus ROG Strix 3080 O10G Gaming"),
@@ -492,6 +493,7 @@ const Asus3000GPUIDs =
 	new AsusGPUIdentifier(Nvidia.RTX3090TI, AsusID.RTX3090TI_TUF_GAMING, "Asus TUF 3090TI Gaming"),
 
 	new AsusGPUIdentifier(Nvidia.RTX4070TI, AsusID.RTX4070TI_TUF_GAMING_OC, "Asus TUF RTX 4070Ti Gaming OC"),
+	new AsusGPUIdentifier(Nvidia.RTX4070TI, AsusID.RTX4070TI_TUF_GAMING_OC_2, "Asus TUF RTX 4070Ti Gaming OC"),
 	new AsusGPUIdentifier(Nvidia.RTX4080, AsusID.RTX4080_TUF_GAMING, "Asus TUF RTX 4080 Gaming"),
 	new AsusGPUIdentifier(Nvidia.RTX4080, AsusID.RTX4080_STRIX_GAMING_OC, "Asus ROG Strix 4080 Gaming OC"),
 	new AsusGPUIdentifier(Nvidia.RTX4080, AsusID.RTX4080_TUF_GAMING_OC, "Asus TUF RTX 4080 Gaming OC"),
