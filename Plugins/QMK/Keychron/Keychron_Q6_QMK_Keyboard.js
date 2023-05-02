d22859702b0fa6365f509269a332de8afd51d224
Guilherme <guicandido1337@hotmail.com>
2023-05-02 18:47:16 -0300

    Q6 merge

--- Plugins/QMK/Keychron/Keychron_Q6_QMK_Keyboard.js
+++ Plugins/QMK/Keychron/Keychron_Q6_QMK_Keyboard.js
@@ -1,6 +1,7 @@
 export function Name() { return "Keychron Q6 QMK Keyboard"; }
+export function Version() { return "1.1.2"; }
 export function VendorId() { return 0x3434; }
-export function ProductId() { return [0x0160, 0x0163]; }
+export function ProductId() { return [0x0160, 0x0161, 0x0162, 0x0163]; }
 export function Publisher() { return "WhirlwindFX"; }
 export function Documentation(){ return ""; }
 export function Size() { return [21, 6]; }
@@ -11,6 +12,7 @@ shutdownColor:readonly
 LightingMode:readonly
 forcedColor:readonly
 */
+
 export function ControllableParameters() {
 	return [
 		{"property":"shutdownColor", "group":"lighting", "label":"Shutdown Color", "min":"0", "max":"360", "type":"color", "default":"#009bde"},
@@ -19,35 +21,69 @@ export function ControllableParameters() {
 	];
 }
 
+let vKeyNames = [];
+let vKeyPositions = [];
+let vKeys = [];
+
 //Plugin Version: Built for Protocol V1.0.4
-
-const vKeys =
+const vKeysANSI =
 [
-	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,		  13, 14, 15,      16, 17, 18, 19, //20
-	20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,  33,  34, 35, 36,      37, 38, 39,
-	40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52,  53,  54, 55, 56,      57, 58, 59, 60,
-	61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72,     73,                 74, 75, 76, 77,
-	78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89,             90,         91, 92, 93,
-	94, 95, 96, 97,         98, 99, 100, 101, 102, 103, 104,    105, 106, 107,
-];
-const vKeyNames =
-[
-	"Esc",     "F1", "F2", "F3", "F4",   "F5", "F6", "F7", "F8",    "F9", "F10", "F11", "F12",  "Print Screen", "Scroll Lock", "Pause Break",   "Circle", "Triangle", "Square", "Cross", //What is this a playstation controller?
-	"`", "1",  "2", "3", "4", "5",  "6", "7", "8", "9", "0",  "-",   "+",  "Backspace",        "Insert",        "Home",     "Page Up",   "NumLock", "Num /", "Num *", "Num -",
-	"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\",                        "Del",         "End",   "Page Down",  "Num 7", "Num 8", "Num 9",
-	"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter",                                                           "Num 4", "Num 5", "Num 6", "Num +",
-	"Left Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Right Shift",                           "Up Arrow",                  "Num 1", "Num 2", "Num 3",
-	"Left Ctrl", "Left Win", "Left Alt", "Space", "Right Alt", "Fn", "Menu", "Right Ctrl",  "Left Arrow",  "Down Arrow", "Right Arrow",   "Num 0", "Num .", "Num Enter",
+	0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 		13, 14, 15,    16, 17, 18, 19,
+	20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,  33, 	34, 35, 36,    37, 38, 39, 40,
+	41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54,		55, 56, 57,    58, 59, 60,
+	60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 				   74, 75, 76, 77,
+	78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 				90,  	   91, 92, 93,
+	95,	96, 97, 			98, 		99, 100, 101, 102,  	103, 104, 105, 106, 107
 ];
 
-const vKeyPositions =
+const vKeyNamesANSI =
 [
-	[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0],       [11, 0], [12, 0],  [13, 0],  [14, 0], [15, 0], [16, 0],  [17, 0], [18, 0], [19, 0], [20, 0],
-	[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1],  [13, 1],  [14, 1], [15, 1], [16, 1],  [17, 1], [18, 1], [19, 1], [20, 1],
-	[0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2],  [13, 2],  [14, 2], [15, 2], [16, 2],  [17, 2], [18, 2], [19, 2],
-	[0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3],          [13, 3],                          [17, 3], [18, 3], [19, 3], [20, 3],
-	[0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [11, 4],                           [15, 4],         [17, 4], [18, 4], [19, 4],
-	[0, 5], [1, 5], [2, 5],                  [3, 5],                  [10, 5], [11, 5], [12, 5],  [13, 5],  [14, 5], [15, 5], [16, 5],  [17, 5], [18, 5],       [20, 4]
+	"Esc",	"F1", "F2", "F3", "F4",	"F5", "F6", "F7", "F8", "F9",	"F10", "F11", "F12", 			"Print Screen", "Scroll Lock", "Pause Break",	"Circle", "Triangle", "Square", "Cross",
+	"`",   "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+",		  "Backspace",        	"Insert", "Home", "Page Up",         			"NumLock", "Num /", "Num *", "Num -",
+	"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\",	            		"Del", "End", "Page Down",          		    "Num 7", "Num 8", "Num 9",
+	"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter",											           					"Num 4", "Num 5", "Num 6", "Num +",
+	"Left Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/",	"Right Shift",									"Up Arrow",		   		  		"Num 1", "Num 2", "Num 3",
+	"Left Ctrl", "Left Win", "Left Alt", "Space", "Right Alt", "Fn", "Menu", "Right Ctrl", 			"Left Arrow", "Down Arrow", "Right Arrow",		"Num 0", "Num .", "Num Enter",
+];
+
+const vKeyPositionsANSI =
+[
+	[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0],	[10, 0], [11, 0], [12, 0], 			[14, 0], [15, 0], [16, 0], [17, 0], [18, 0], [19, 0], [20, 0],
+	[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [13, 1], [14, 1], [15, 1], [16, 1], [17, 1], [18, 1], [19, 1], [20, 1],
+	[0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], [13, 2],	[14, 2], [15, 2], [16, 2], [17, 2], [18, 2], [19, 2],
+	[0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3], 		   [13, 3],	 						   [17, 3], [18, 3], [19, 3], [20, 3],
+	[0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [11, 4],		                     [15, 4],	       [17, 4], [18, 4], [19, 4],
+	[0, 5], [1, 5], [2, 5], 				[5, 5], 						[9, 5], [10, 5], [11, 5], [12, 5],	  		[14, 5], [15, 5], [16, 5], [17, 5], [18, 5], 		  [20, 5],
+];
+
+const vKeysISO =
+[
+	0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 		13, 14, 15,    16, 17, 18, 19,
+	20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,  33, 	34, 35, 36,    37, 38, 39, 40,
+	41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,  		54, 55, 56,    57, 58, 59,
+	60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 				   74, 75, 76, 77,
+	78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90,  			91,        92, 93, 94,
+	95,	96, 97, 			98, 		99, 100, 101, 102,  	103, 104, 105,  106, 107, 108,
+];
+
+const vKeyNamesISO =
+[
+	"Esc",	"F1", "F2", "F3", "F4",	"F5", "F6", "F7", "F8", "F9",	"F10", "F11", "F12", 			"Print Screen", "Scroll Lock", "Pause Break",	"Circle", "Triangle", "Square", "Cross",
+	"`",   "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "+",		  "Backspace",        	"Insert", "Home", "Page Up",         			"NumLock", "Num /", "Num *", "Num -",
+	"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]",			            		"Del", "End", "Page Down",          		    "Num 7", "Num 8", "Num 9",
+	"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "ISO_#",	"Enter",											           		"Num 4", "Num 5", "Num 6", "Num +",
+	"Left Shift", "ISO_<", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/",	"Right Shift",				"Up Arrow",		   		  		    	"Num 1", "Num 2", "Num 3",
+	"Left Ctrl", "Left Win", "Left Alt", "Space", "Right Alt", "Fn", "Menu", "Right Ctrl", 			"Left Arrow", "Down Arrow", "Right Arrow",		"Num 0", "Num .", "Num Enter",
+];
+
+const vKeyPositionsISO =
+[
+	[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0],	[10, 0], [11, 0], [12, 0], 			[14, 0], [15, 0], [16, 0], [17, 0], [18, 0], [19, 0], [20, 0],
+	[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [13, 1], [14, 1], [15, 1], [16, 1], [17, 1], [18, 1], [19, 1], [20, 1],
+	[0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], 			[14, 2], [15, 2], [16, 2], [17, 2], [18, 2], [19, 2],
+	[0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3], [12, 3], [13, 3],	 						   [17, 3], [18, 3], [19, 3], [20, 3],
+	[0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [11, 4], [12, 4],                   [15, 4],	       [17, 4], [18, 4], [19, 4],
+	[0, 5], [1, 5], [2, 5], 				[5, 5], 						[9, 5], [10, 5], [11, 5], [12, 5],	  		[14, 5], [15, 5], [16, 5], [17, 5], [18, 5], 		  [20, 5],
 ];
 
 let LEDCount = 0;
@@ -57,14 +93,31 @@ const VIAFirmware = 2;
 const PluginProtocolVersion = "1.0.4";
 
 export function LedNames() {
-	return vKeyNames;
+	return vKeyNamesANSI;
 }
 
 export function LedPositions() {
-	return vKeyPositions;
+	return vKeyPositionsANSI;
 }
 
 export function Initialize() {
+
+	if ([0x0160, 0x0161].includes(device.productId())) {
+		vKeyNames = vKeyNamesANSI;
+		vKeyPositions = vKeyPositionsANSI;
+		vKeys = vKeysANSI;
+
+		device.log(`Layout changed to ANSI`);
+		device.setControllableLeds(vKeyNames, vKeyPositions);
+	} else {
+		vKeyNames = vKeyNamesISO;
+		vKeyPositions = vKeyPositionsISO;
+		vKeys = vKeysISO;
+
+		device.log(`Layout changed to ISO`);
+		device.setControllableLeds(vKeyNames, vKeyPositions);
+	}
+
 	requestFirmwareType();
 	requestQMKVersion();
 	requestSignalRGBProtocolVersion();
@@ -78,8 +131,14 @@ export function Render() {
 	sendColors();
 }
 
-export function Shutdown() {
-	effectDisable();
+export function Shutdown(SystemSuspending) {
+
+	if(SystemSuspending){
+		sendColors("#000000"); // Go Dark on System Sleep/Shutdown
+	}else{
+		effectDisable();
+	}
+
 }
 
 function commandHandler() {
@@ -136,6 +195,7 @@ function returnQMKVersion(data) {
 	const QMKVersionByte2 = data[3];
 	const QMKVersionByte3 = data[4];
 	device.log("QMK Version: " + QMKVersionByte1 + "." + QMKVersionByte2 + "." + QMKVersionByte3);
+	device.log("QMK SRGB Plugin Version: "+ Version());
 	device.pause(30);
 }
 
@@ -164,16 +224,24 @@ function returnSignalRGBProtocolVersion(data) {
 
 function requestUniqueIdentifier() //Grab the unique identifier for this keyboard model
 {
-	device.write([0x00, 0x23], 32);
+	if(device.write([0x00, 0x23], 32) === -1) {
+		device.notify("Unsupported Firmware: ", `This device is not running SignalRGB-compatible firmware. Click the Open Troubleshooting Docs button to learn more.`, 0);
+	}
+
 	device.pause(30);
 	commandHandler();
 }
+
 
 function returnUniqueIdentifier(data) {
 	const UniqueIdentifierByte1 = data[2];
 	const UniqueIdentifierByte2 = data[3];
 	const UniqueIdentifierByte3 = data[4];
-	device.log("Unique Device Identifier: " + UniqueIdentifierByte1 + UniqueIdentifierByte2 + UniqueIdentifierByte3);
+
+	if(!(UniqueIdentifierByte1 === 0 && UniqueIdentifierByte2 === 0 && UniqueIdentifierByte3 === 0)) {
+		device.log("Unique Device Identifier: " + UniqueIdentifierByte1 + UniqueIdentifierByte2 + UniqueIdentifierByte3);
+	}
+
 	device.pause(30);
 }
 
@@ -199,15 +267,20 @@ function requestFirmwareType() {
 function returnFirmwareType(data) {
 	const FirmwareTypeByte = data[2];
 
-	if(FirmwareTypeByte !== MainlineQMKFirmware || FirmwareTypeByte !== VIAFirmware) {
+	if(!(FirmwareTypeByte === MainlineQMKFirmware || FirmwareTypeByte === VIAFirmware)) {
 		device.notify("Unsupported Firmware: ", "Click Show Console, and then click on troubleshooting for your keyboard to find out more.", 0);
 	}
 
-	if(FirmwareTypeByte === VIAFirmware){
-		IsViaKeyboard = true;
+	if(FirmwareTypeByte === MainlineQMKFirmware) {
+		IsViaKeyboard = false;
+		device.log("Firmware Type: Mainline");
 	}
 
-	device.log("Firmware Type: " + FirmwareTypeByte);
+	if(FirmwareTypeByte === VIAFirmware) {
+		IsViaKeyboard = true;
+		device.log("Firmware Type: VIA");
+	}
+
 	device.pause(30);
 }
 
@@ -223,7 +296,7 @@ function effectDisable() //Revert to Hardware Mode
 	device.pause(30);
 }
 
-function grabColors(shutdown = false) {
+function grabColors(overrideColor) {
 	const rgbdata = [];
 
 	for(let iIdx = 0; iIdx < vKeys.length; iIdx++) {
@@ -231,8 +304,8 @@ function grabColors(shutdown = false) {
 		const iPxY = vKeyPositions[iIdx][1];
 		let color;
 
-		if(shutdown) {
-			color = hexToRgb(shutdownColor);
+		if(overrideColor) {
+			color = hexToRgb(overrideColor);
 		} else if (LightingMode === "Forced") {
 			color = hexToRgb(forcedColor);
 		} else {
@@ -248,8 +321,8 @@ function grabColors(shutdown = false) {
 	return rgbdata;
 }
 
-function sendColors() {
-	const rgbdata = grabColors();
+function sendColors(overrideColor) {
+	const rgbdata = grabColors(overrideColor);
 
 	const LedsPerPacket = 9;
 	let BytesSent = 0;
