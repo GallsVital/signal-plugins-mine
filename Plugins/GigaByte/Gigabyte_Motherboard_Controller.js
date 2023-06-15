1124dc421cd5edb8b2ee863c7b39bccd87ee86d9
Guilherme <guicandido1337@hotmail.com>
2023-06-15 09:54:59 -0300

    Trying all leds

--- Plugins/GigaByte/Gigabyte_Motherboard_Controller.js
+++ Plugins/GigaByte/Gigabyte_Motherboard_Controller.js
@@ -149,8 +149,11 @@ const MotherboardConfigs = {
 			0x20: ["Back IO", MainboardConfiguration],
 			0x21: ["Mainboard", MainboardConfiguration],
 			0x22: ["PCIe", MainboardConfiguration],
-			0x23: ["South Bridge", HeaderConfiguration],
+			0x23: ["South Bridge", MainboardConfiguration],
 			0x24: ["12v Header", HeaderConfiguration],
+			0x25: ["Led 4", MainboardConfiguration],
+			0x26: ["Led 5", MainboardConfiguration],
+			0x27: ["Led 6", MainboardConfiguration],
 		}
 	},
 	"B760M AORUS ELITE AX": {
