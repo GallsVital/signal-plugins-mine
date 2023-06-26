3f0e9a3c071e04804ff84beee62f3b6d5d40d1f6
TheDordo <ripet1634@gmail.com>
2023-06-26 14:25:17 -0400

    New 4070TI TUF PID

--- Plugins/Asus/GPUs/Asus_Ampere_Lovelace_GPU.js
+++ Plugins/Asus/GPUs/Asus_Ampere_Lovelace_GPU.js
@@ -524,8 +524,9 @@ class Asus_Ampere_Lovelace_IDs {
 		this.RTX4070TI_12GB_STRIX_GAMING_OC		   = 0X88A7;
 		this.RTX4070TI_12GB_STRIX_GAMING_OC_2      = 0x88DC;
 		this.RTX4070TI_TUF_GAMING				   = 0x88A6;
-
 		this.RTX4070TI_TUF_GAMING_OC_2			   = 0x88A4;
+		this.RTX4070TI_12GB_TUF					   = 0x88dd;
+
 		this.RTX4080_STRIX_GAMING_3				   = 0x88A0;
 		this.RTX4080_TUF_GAMING					   = 0x88A1;
 		this.RTX4080_STRIX_GAMING				   = 0x889f;
@@ -643,11 +644,14 @@ const Asus3000GPUIDs =
 	new AsusGPUIdentifier(Nvidia.RTX3090TI, AsusID.RTX3090TI_TUF_GAMING, "Asus TUF 3090TI Gaming"),
 
 	new AsusGPUIdentifier(Nvidia.RTX4070, AsusID.RTX4070_TUF_GAMING, "Asus TUF RTX 4070 Gaming"),
+
 	new AsusGPUIdentifier(Nvidia.RTX4070TI, AsusID.RTX4070TI_12GB_STRIX_GAMING_OC, "Asus ROG Strix RTX 4070Ti 12GB Gaming OC"),
 	new AsusGPUIdentifier(Nvidia.RTX4070TI, AsusID.RTX4070TI_12GB_STRIX_GAMING_OC_2, "Asus ROG Strix RTX 4070Ti 12GB Gaming OC"),
 	new AsusGPUIdentifier(Nvidia.RTX4070TI, AsusID.RTX4070TI_TUF_GAMING_OC, "Asus TUF RTX 4070Ti Gaming OC"),
 	new AsusGPUIdentifier(Nvidia.RTX4070TI, AsusID.RTX4070TI_TUF_GAMING_OC_2, "Asus TUF RTX 4070Ti Gaming OC"),
 	new AsusGPUIdentifier(Nvidia.RTX4070TI, AsusID.RTX4070TI_TUF_GAMING, "Asus TUF RTX 4070Ti 12GB Gaming"),
+	new AsusGPUIdentifier(Nvidia.RTX4070TI, AsusID.RTX4070TI_12GB_TUF, "Asus TUF RTX 4070Ti 12GB Gaming"),
+
 	new AsusGPUIdentifier(Nvidia.RTX4080, AsusID.RTX4080_TUF_GAMING, "Asus TUF RTX 4080 Gaming"),
 	new AsusGPUIdentifier(Nvidia.RTX4080, AsusID.RTX4080_TUF_GAMING_2, "Asus TUF RTX 4080 Gaming"),
 	new AsusGPUIdentifier(Nvidia.RTX4080, AsusID.RTX4080_TUF_GAMING_3, "Asus TUF RTX 4080 Gaming"),
