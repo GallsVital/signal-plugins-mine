3b2fb366c9c110e460e977db125ad48d70e9e473
Guilherme <guicandido1337@hotmail.com>
2023-08-19 01:58:35 -0300

    Device to Controller update

--- Plugins/Asus/Asus_Aura_Terminal_Controller.js
+++ Plugins/Asus/Asus_Aura_Terminal_Controller.js
@@ -1,25 +1,54 @@
 export function Name() { return "Asus Aura Terminal"; }
-export function VendorId() { return 0x0b05; }
+export function VendorId() { return 0x0B05; }
 export function ProductId() { return 0x1889; }
 export function Publisher() { return "WhirlwindFX"; }
-export function Size() { return [5, 1]; }
+export function Documentation(){ return "troubleshooting/asus"; }
+export function Size() { return [5, 5]; }
 export function DefaultPosition(){return [240, 120];}
 export function DefaultScale(){return 8.0;}
+export function SubdeviceController(){ return true; }
+export function ConflictingProcesses() { return ["LightingService.exe"]; }
 /* global
 shutdownColor:readonly
 LightingMode:readonly
 forcedColor:readonly
+RGBconfig0:readonly
+RGBconfig1:readonly
+RGBconfig2:readonly
+RGBconfig3:readonly
 */
 export function ControllableParameters(){
 	return [
 		{"property":"shutdownColor", "group":"lighting", "label":"Shutdown Color", "min":"0", "max":"360", "type":"color", "default":"#009bde"},
 		{"property":"LightingMode", "group":"lighting", "label":"Lighting Mode", "type":"combobox", "values":["Canvas", "Forced"], "default":"Canvas"},
 		{"property":"forcedColor", "group":"lighting", "label":"Forced Color", "min":"0", "max":"360", "type":"color", "default":"#009bde"},
+		{"property":"RGBconfig0", "group":"lighting", "label":"RGB Header Channel 1", "type":"combobox", "values":["RGB", "RBG", "BGR", "BRG", "GBR", "GRB"], "default":"RGB"},
+		{"property":"RGBconfig1", "group":"lighting", "label":"RGB Header Channel 2", "type":"combobox", "values":["RGB", "RBG", "BGR", "BRG", "GBR", "GRB"], "default":"RGB"},
+		{"property":"RGBconfig2", "group":"lighting", "label":"RGB Header Channel 3", "type":"combobox", "values":["RGB", "RBG", "BGR", "BRG", "GBR", "GRB"], "default":"RGB"},
+		{"property":"RGBconfig3", "group":"lighting", "label":"RGB Header Channel 4", "type":"combobox", "values":["RGB", "RBG", "BGR", "BRG", "GBR", "GRB"], "default":"RGB"},
 	];
 }
 
-const vLedNames = [ "Zone 1", "Zone 2", "Zone 3", "Zone 4", "Logo" ];
-const vLedPositions = [ [0, 0], [1, 0], [2, 0], [3, 0], [4, 0] ];
+const DeviceLedLimit = 210;
+
+const ChannelArray = [
+	["Channel 1", 90],
+	["Channel 2", 90],
+	["Channel 3", 90],
+	["Channel 4", 90],
+];
+
+const RGBConfigs = {
+	"RGB" : [0, 1, 2],
+	"RBG" : [0, 2, 1],
+	"BGR" : [2, 1, 0],
+	"BRG" : [2, 0, 1],
+	"GBR" : [1, 2, 0],
+	"GRB" : [1, 0, 2]
+};
+
+const vLedNames = ["Logo"];
+const vLedPositions = [[2, 2]];
 
 export function LedNames() {
 	return vLedNames;
@@ -29,60 +58,98 @@ export function LedPositions() {
 	return vLedPositions;
 }
 
-
 export function Initialize() {
+	SetupChannels();
+}
+
+export function Render() {
+	sendColors();
+}
+
+export function Shutdown(SystemSuspending) {
+
+	if(SystemSuspending){
+		sendColors("#000000"); // Go Dark on System Sleep/Shutdown
+	}else{
+		sendColors(shutdownColor);
+	}
 
 }
 
-function sendZone(zone, shutdown = false) {
-	const packet = [];
-	//packet[0] = 0x00;
-	packet[0] = 0xec;
-	packet[1] = 0x3b;
-	packet[2] = zone;
-	packet[3] = 0x00;
-	packet[4] = 0x01;//Might be number of zones flag
+function SetupChannels(){
 
+	// Set 4 channels and Logo to direct mode
+	for(let i = 0; i < 5; i++){
+		device.write([0xEC, 0x3B, i, 0x00, 0xFF], 65);
+	}
 
-	const iX = vLedPositions[zone][0];
-	const iY = vLedPositions[zone][1];
+	for(let i = 0; i < ChannelArray.length; i++) {
+		device.addChannel(ChannelArray[i][0], ChannelArray[i][1]);
+	}
+
+	device.SetLedLimit(DeviceLedLimit);
+}
+
+function sendColors(overrideColor) {
+
+	// Get channels RGB
+	for(let i = 0; i < ChannelArray.length; i++) {
+		const RGBData = grabRGBData(i, overrideColor);
+		const LedCountChannel = device.channel(ChannelArray[i][0]).LedCount();
+		let LedCount = Math.min(LedCountChannel, DeviceLedLimit);
+		let ledsSent = 0;
+
+		// No led on this channel, skip
+		if(!RGBData.length) {continue;}
+
+		while(LedCount > 0){
+			// Gate Led count to max per packet size
+			const ledsToSend = Math.min(LedCount, 20);
+			LedCount -= ledsToSend;
+			device.write([0xEC, 0x40, i, ledsSent, ledsToSend].concat(RGBData.splice(0, ledsToSend*3)), 65);
+			ledsSent += ledsToSend;
+		}
+
+		device.write([0xEC, 0x40, (128 + i)], 65);
+	}
+
+	// Logo LED
+	const iX = vLedPositions[0][0];
+	const iY = vLedPositions[0][1];
 	let color;
 
-	if(shutdown) {
-		color = hexToRgb(shutdownColor);
+	if(overrideColor) {
+		color = hexToRgb(overrideColor);
 	} else if (LightingMode == "Forced") {
 		color = hexToRgb(forcedColor);
 	} else {
 		color = device.color(iX, iY);
 	}
 
-	packet[5] = color[0];
-	packet[6] = color[1];
-	packet[7] = color[2];
-
-
-	device.write(packet, 65);
-	device.pause(5);
+	device.write([0xEC, 0x40, 0x04, 0x00, 0x01, color[0], color[1], color[2]], 65);
+	device.write([0xEC, 0x40, 0x84], 65);
 }
 
-export function Render() {
-	sendZone(0);
-	sendZone(1);
-	sendZone(2);
-	sendZone(3);
-	sendZone(4);
-}
+function grabRGBData(Channel, overrideColor) {
 
-export function Shutdown() {
-	sendZone(0, true);
-	sendZone(1, true);
-	sendZone(2, true);
-	sendZone(3, true);
-	sendZone(4, true);
-}
+	let ChannelLedCount = device.channel(ChannelArray[Channel][0]).LedCount();
+	const componentChannel = device.channel(ChannelArray[Channel][0]);
+	let RGBData = [];
 
-export function Validate(endpoint) {
-	return endpoint.interface === -1;
+	if(overrideColor) {
+		RGBData = device.createColorArray(overrideColor, ChannelLedCount, "Inline", RGBConfigs["RGBconfig" + Channel]);
+	} else if(LightingMode === "Forced") {
+		RGBData = device.createColorArray(forcedColor, ChannelLedCount, "Inline", RGBConfigs["RGBconfig" + Channel]);
+	} else if(componentChannel.shouldPulseColors()) {
+		ChannelLedCount = ChannelArray[Channel][1];
+
+		const pulseColor = device.getChannelPulseColor(ChannelArray[Channel][0]);
+		RGBData = device.createColorArray(pulseColor, ChannelLedCount, "Inline", RGBConfigs["RGBconfig" + Channel]);
+	} else {
+		RGBData = device.channel(ChannelArray[Channel][0]).getColors("Inline", RGBConfigs["RGBconfig" + Channel]);
+	}
+
+	return RGBData;
 }
 
 function hexToRgb(hex) {
@@ -95,6 +162,10 @@ function hexToRgb(hex) {
 	return colors;
 }
 
-export function Image() {
-	return "";
+export function Validate(endpoint) {
+	return endpoint.interface === 0;
+}
+
+export function ImageUrl() {
+	return "https://dlcdnwebimgs.asus.com/gain/52914235-AAED-4F08-94D6-BBB255D9BFB6/w1000/h732.png";
 }
