c42f9df07b2d5d0f0da10d84364639262d4688d0
Guilherme <guicandido1337@hotmail.com>
2023-09-01 17:44:18 -0300

    Scope II 96 base working

--- Plugins/Asus/Asus_Scope_Keyboard.js
+++ Plugins/Asus/Asus_Scope_Keyboard.js
@@ -19,11 +19,15 @@ export function ControllableParameters() {
 	];
 }
 
+let savedPollTimer = Date.now();
+const PollModeInternal = 15000;
+
 export function Initialize() {
 	ASUS.InitializeASUS();
 }
 
 export function Render() {
+	ASUS.getDeviceBatteryStatus();
 	ASUS.sendColors();
 }
 
@@ -47,6 +51,10 @@ export class ASUS_Mouse_Protocol {
 			Leds: [],
 			LedNames: [],
 			LedPositions: [],
+			SupportedFeatures:
+			{
+				BatterySupport: false,
+			}
 		};
 	}
 
@@ -61,6 +69,9 @@ export class ASUS_Mouse_Protocol {
 	getDeviceEndpoint() { return this.Config.DeviceEndpoint; }
 	setDeviceEndpoint(deviceEndpoint) { this.Config.DeviceEndpoint = deviceEndpoint; }
 
+	getLedsPerPacket() {return this.Config.ledsPerPacket; }
+	setLedsPerPacket(ledsperpacket) { this.Config.ledsPerPacket = ledsperpacket; }
+
 	getLeds() { return this.Config.Leds; }
 	setLeds(leds) { this.Config.Leds = leds; }
 
@@ -69,6 +80,9 @@ export class ASUS_Mouse_Protocol {
 
 	getLedPositions() { return this.Config.LedPositions; }
 	setLedPositions(ledPositions) { this.Config.LedPositions = ledPositions; }
+
+	getBatteryFeature() { return this.Config.SupportedFeatures.BatterySupport; }
+	setBatteryFeature(battery) { this.Config.SupportedFeatures.BatterySupport = battery; }
 
 	getDeviceImage(deviceName) { return ASUSdeviceLibrary.imageLibrary[deviceName]; }
 
@@ -79,8 +93,17 @@ export class ASUS_Mouse_Protocol {
 
 		const DeviceProperties = this.getDeviceProperties(this.getDeviceName());
 		this.setDeviceEndpoint(DeviceProperties.endpoint);
+		this.setLeds(DeviceProperties.vLeds);
 		this.setLedNames(DeviceProperties.vLedNames);
 		this.setLedPositions(DeviceProperties.vLedPositions);
+
+		if(DeviceProperties.battery){
+			this.setBatteryFeature(true);
+
+			device.addFeature("battery");
+			console.log("Device has a battery and it's wireless");
+
+		}
 
 		device.log(`Device model found: ` + this.getDeviceName());
 		device.setName("ASUS " + this.getDeviceName());
@@ -127,9 +150,36 @@ export class ASUS_Mouse_Protocol {
 			packet[4] = 0x00;
 			packet = packet.concat(RGBData.splice(0, ledsToSend*4));
 			device.write(packet, 65);
-			//device.read(packet,65)
 			TotalLedCount -= ledsToSend;
 		}
+	}
+
+	getDeviceBatteryStatus() {
+
+		if(this.getBatteryFeature()){
+			if (Date.now() - savedPollTimer < PollModeInternal) {
+				return;
+			}
+
+			console.log("Device has battery, polling info...");
+			savedPollTimer = Date.now();
+
+			this.modernFetchBatteryLevel();
+		}
+
+	}
+
+	modernFetchBatteryLevel() {
+		device.clearReadBuffer();
+		device.write([0x00, 0x12, 0x07], 65);
+
+		const returnPacket = device.read([0x00, 0x12, 0x07], 65);
+
+		const batteryState = returnPacket[4];
+		const batteryLevel = returnPacket[5];
+
+		battery.setBatteryLevel(batteryLevel);
+		battery.setBatteryState(batteryState + 1);
 	}
 }
 
@@ -175,6 +225,7 @@ export class deviceLibrary {
 					[0, 5], [1, 5], [2, 5],                      [6, 5],                        [10, 5], [11, 5],  [12, 5], [13, 5],    [14, 5], [15, 5], [16, 5],   [17, 5],         [19, 5],               // 13
 				],
 				endpoint : { "interface": 1, "usage": 0x0001, "usage_page": 0xFF00, "collection": 0x0000 },
+
 			},
 			"ROG Strix Scope TKL":
 			{
@@ -211,6 +262,7 @@ export class deviceLibrary {
 					[0, 6], [0, 6], [1, 6], [1, 6], [2, 6], [3, 6], [4, 6], [4, 6], [5, 6],  [5, 6], [6, 6], [7, 6], [7, 6], [8, 6], [9, 6], [9, 6], [10, 6], [11, 6], [11, 6], [12, 6], [13, 6], [13, 6], [14, 6], [15, 6], [16, 6], [16, 6]
 				],
 				endpoint : { "interface": 1, "usage": 0x0001, "usage_page": 0xFF00, "collection": 0x0000 },
+
 			},
 			"ROG Strix Scope RX":
 			{
@@ -240,35 +292,37 @@ export class deviceLibrary {
 					[0, 5], [1, 5], [2, 5],                      [6, 5],                        [10, 5], [11, 5],  [12, 5], [13, 5],    [14, 5], [15, 5], [16, 5],   [17, 5],         [19, 5],               // 13
 				],
 				endpoint : { "interface": 1, "usage": 0x0001, "usage_page": 0xFF00, "collection": 0x0000 },
+
 			},
 			"ROG Strix Scope II 96":
 			{
-				size: [21, 6],
+				size: [17, 6],
 				vLeds:[
-					0,      24, 32, 40, 48,   64, 72, 80, 88,  96, 104, 112, 120,	128, 136, 144,
-					1,  17, 25, 33, 41, 49, 57, 65, 73, 81, 89, 97, 105,    121,	129, 137, 145,	153, 161, 169, 177,
-					2,  18, 26, 34, 42, 50, 58, 66, 74, 82, 90, 98, 106,    122,	130, 138, 146,	154, 162, 170, 178,
-					3,    19, 27, 35, 43, 51, 59, 67, 75, 83, 91, 99,      123,						155, 163, 171,
-					4,    20, 28, 36, 44, 52, 60, 68, 76, 84, 92,          124,			140,		156, 164, 172, 180,
-					5,    21, 29,      53,            77, 93, 101,  125,   133, 	141, 149, 157,	173,
+					0, 24, 32, 40, 48, 64, 72, 80, 88, 96, 104, 112, 120, 128, 136, 144, 142, //17
+					1, 17, 25, 33, 41, 49, 57, 65, 73, 81, 89, 97, 105, 121, 129, 137, 145,	153, //18
+					2, 18, 26, 34, 42, 50, 58, 66, 74, 82, 90, 98, 106, 122, 130, 138, 146,	154, //18
+					3, 19, 27, 35, 43, 51, 59, 67, 75, 83, 91, 99, 123, 155, 163, 171, //16
+					4, 20, 28, 36, 44, 52, 60, 68, 76, 84, 92, 124, 140, 156, 164, 172, 180, //17
+					5, 21, 29,			53,            77, 93, 101,  125, 133, 141, 149, 157, //12
 				],
 				vLedNames: [
-					"Esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",         "Print Screen", "Scroll Lock", "Pause Break",
-					"`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-_", "=+", "Backspace",                        "Insert", "Home", "Page Up",       "NumLock", "Num /", "Num *", "Num -",  //21
-					"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\",                               "Del", "End", "Page Down",         "Num 7", "Num 8", "Num 9", "Num +",    //21
-					"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter",                                                              "Num 4", "Num 5", "Num 6",             //16
-					"Left Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Right Shift",                                  "Up Arrow",               "Num 1", "Num 2", "Num 3", "Num Enter", //17
-					"Left Ctrl", "Left Win", "Left Alt", "Space", "Right Alt", "Fn", "Menu", "Right Ctrl",  "Left Arrow", "Down Arrow", "Right Arrow", "Num 0", "Num ."                       //13
+					"Esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12", "Ins", "Del", "PgUp", "PgDn", //17
+					"`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-_", "=+", "Backspace", "Num", "Num /", "Num *", "Num -", //18
+					"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\", 		"Num 7", "Num 8", "Num 9", "Num +", //18
+					"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter",		"Num 4", "Num 5", "Num 6", //16
+					"Left Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "RS", "Up Arrow", "Num 1", "Num 2", "Num 3", "Num Enter", //17
+					"LCtrl", "LWin", "LAlt", "Space", "RAlt", "Fn", "RCtrl", "Left Arrow", "Down Arrow", "Right Arrow", "Num 0", "Num ." //12
 				],
 				vLedPositions: [
-					[0, 0],    [1, 0], [2, 0], [3, 0], [4, 0],    [6, 0], [7, 0], [8, 0], [9, 0],  [10, 0], [11, 0], [12, 0], [13, 0],      [14, 0], [15, 0], [16, 0],            //20
-					[0, 1],  [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [13, 1],     [14, 1], [15, 1], [16, 1],   [17, 1], [18, 1], [19, 1], [20, 1], //21
-					[0, 2],    [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], [13, 2],   [14, 2], [15, 2], [16, 2],   [17, 2], [18, 2], [19, 2], [20, 3], //20
-					[0, 3],    [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3],         [13, 3],                             [17, 3], [18, 3], [19, 3], // 17
-					[0, 4],      [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4],               [13, 4],           [15, 4],           [17, 4], [18, 4], [19, 4], [20, 4], // 17
-					[0, 5], [1, 5], [2, 5],                      [6, 5],                        [10, 5], [11, 5],  [12, 5], [13, 5],    [14, 5], [15, 5], [16, 5],   [17, 5],         [19, 5],               // 13
+					[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [ 9, 0], [10, 0], [11, 0], [12, 0], [13, 0], [14, 0], [15, 0], //17
+					[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [ 9, 1], [10, 1], [11, 1], [12, 1], [13, 1], [14, 1], [15, 1], [16, 1], //18
+					[0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [ 9, 2], [10, 2], [11, 2], [12, 2], [13, 2], [14, 2], [15, 2], [16, 2], //18
+					[0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [ 9, 3], [10, 3], [11, 3], [12, 3], [13, 3], [14, 3], //16
+					[0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [ 9, 4], [10, 4], [11, 4], [12, 4], [13, 4], [14, 4], [15, 4], //17
+					[0, 5], [1, 5], [2, 5],					[5, 5],                         [ 9, 5], [10, 5], [11, 5], [12, 5], [13, 5], [14, 5], [15, 5], [16, 5], //12
 				],
 				endpoint : { "interface": 1, "usage": 0x0001, "usage_page": 0xFF00, "collection": 0x0000 },
+				battery: false
 			},
 		};
 
