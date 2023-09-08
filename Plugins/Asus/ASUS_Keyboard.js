5797959587c9db316f8bdb27807d92474f1f9013
Guilherme <guicandido1337@hotmail.com>
2023-09-08 16:45:54 -0300

    Falchion and Flare models added

--- Plugins/Asus/ASUS_Keyboard.js
+++ Plugins/Asus/ASUS_Keyboard.js
@@ -1,4 +1,4 @@
-export function Name() { return "Asus Scope Keyboard"; }
+export function Name() { return "Asus Keyboard"; }
 export function VendorId() { return 0x0B05; }
 export function ProductId() { return Object.keys(ASUSdeviceLibrary.PIDLibrary); }
 export function Publisher() { return "WhirlwindFx"; }
@@ -193,6 +193,13 @@ export class ASUS_Keyboard_Protocol {
 export class deviceLibrary {
 	constructor(){
 		this.PIDLibrary	=	{
+			0x193C: "ROG Falchion", // Wired
+			0x193E: "ROG Falchion",
+			0x1875: "ROG Strix Flare",
+			0x18AF: "ROG Strix Flare",
+			0x18CF: "ROG Strix Flare",
+			0x19FE: "ROG Strix Flare II",
+			0x19FC: "ROG Strix Flare II", // Animate
 			0x18F6: "ROG Strix Scope", // Wireless
 			0x18F8: "ROG Strix Scope",
 			0x190C: "ROG Strix Scope TKL",
@@ -205,6 +212,108 @@ export class deviceLibrary {
 		};
 
 		this.LEDLibrary	=	{
+			"ROG Falchion":
+			{
+				size: [15, 5],
+				vLeds:[
+					0, 8,  16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104,			112,
+					1, 9,  17, 25, 33, 41, 49, 57, 65, 73, 81, 89, 97, 105,			113,
+					2, 10, 18, 26, 34, 42, 50, 58, 66, 74, 82, 90, 98,    106,		114,
+					3, 11, 19, 27, 35, 43, 51, 59, 67, 75, 83, 91, 99,		107,	115,
+					4, 12, 20,         52,         76, 84, 92,			100, 108, 116,
+				],
+				vLedNames: [
+					"Esc", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-_", "=+", "Backspace",								"Insert",
+					"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\",				 /*EntTop*/					"Del",
+					"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "#",					"Enter",				"Page Up",
+					"Left Shift", "ISO\\",  "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Right Shift",		  "Up Arrow",	"Page Down",
+					"Left Ctrl", "Left Win", "Left Alt", "Space", "Right Alt", "Fn", "Right Ctrl",  "Left Arrow", "Down Arrow", "Right Arrow",
+				],
+				vLedPositions: [
+					[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0], [12, 0], [13, 0],	   [14, 0],   //15
+					[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [13, 1],	   [14, 1],   //14
+					[0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2],  [13, 2],   [14, 2],   //15
+					[0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3], [12, 3],   [13, 3],  [14, 3],   //15
+					[0, 4], [1, 4], [2, 4],                         [6, 4],					[9, 4], [10, 4], [11, 4],   [12, 4], [13, 4], [14, 4],    //10
+				],
+				endpoint : { "interface": 1, "usage": 0x0001, "usage_page": 0xFF00, "collection": 0x0000 },
+				LedCount: 144, // Testing purpose
+				LedOffset: 0x90, // Testing purpose
+			},
+			"ROG Strix Flare":
+			{
+				size: [22, 6],
+				vLeds:[
+					0,      24, 32, 40, 48,   64, 72, 80, 88,  96, 104, 112, 120,   128, 136, 144,
+					1,  17, 25, 33, 41, 49, 57, 65, 73, 81, 89, 97, 105,    121,   129, 137, 145,   153, 161, 169, 177,
+					2,  18, 26, 34, 42, 50, 58, 66, 74, 82, 90, 98, 106,    122,   130, 138, 146,   154, 162, 170, 178,
+					3,    19, 27, 35, 43, 51, 59, 67, 75, 83, 91, 99,      123,                  155, 163, 171,
+					4,    20, 28, 36, 44, 52, 60, 68, 76, 84, 92,          124,       140,       156, 164, 172, 180,
+					5,    13, 21,      53,            77, 93, 101,  125,   133, 141, 149,   157,    173,
+
+					184, 185, 186,
+				],
+				vLedNames: [
+					"Esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",         "Print Screen", "Scroll Lock", "Pause Break",
+					"`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-_", "=+", "Backspace",                        "Insert", "Home", "Page Up",       "NumLock", "Num /", "Num *", "Num -",  //21
+					"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\",                               "Del", "End", "Page Down",         "Num 7", "Num 8", "Num 9", "Num +",    //21
+					"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter",                                                              "Num 4", "Num 5", "Num 6",             //16
+					"Left Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Right Shift",                                  "Up Arrow",               "Num 1", "Num 2", "Num 3", "Num Enter", //17
+					"Left Ctrl", "Left Win", "Left Alt", "Space", "Right Alt", "Fn", "Menu", "Right Ctrl",  "Left Arrow", "Down Arrow", "Right Arrow", "Num 0", "Num .",                       //13
+					"Logo", "Left underglow", "Right underglow"
+				],
+				vLedPositions: [
+					[0, 0],    [1, 0], [2, 0], [3, 0], [4, 0],    [6, 0], [7, 0], [8, 0], [9, 0],  [10, 0], [11, 0], [12, 0], [13, 0],      [14, 0], [15, 0], [16, 0],            //20
+					[0, 1],  [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [13, 1],     [14, 1], [15, 1], [16, 1],   [17, 1], [18, 1], [19, 1], [20, 1], //21
+					[0, 2],    [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], [13, 2],   [14, 2], [15, 2], [16, 2],   [17, 2], [18, 2], [19, 2], [20, 3], //20
+					[0, 3],    [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3],         [13, 3],                             [17, 3], [18, 3], [19, 3], // 17
+					[0, 4],  [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4],               [13, 4],           [15, 4],           [17, 4], [18, 4], [19, 4], [20, 4], // 17
+					[0, 5], [1, 5], [2, 5],                      [6, 5],                        [10, 5], [11, 5],  [12, 5], [13, 5],    [14, 5], [15, 5], [16, 5],   [17, 5],         [19, 5],               // 13
+					//Logo,  left underglow, right underglow
+					[18, 0], [0, 2], [21, 2],
+				],
+				endpoint : { "interface": 1, "usage": 0x0001, "usage_page": 0xFF00, "collection": 0x0000 },
+				LedCount: 144, // Testing purpose
+				LedOffset: 0x90, // Testing purpose
+			},
+			"ROG Strix Flare II":
+			{
+				size: [24, 7],
+				vLeds:[
+					0,     24, 32, 40, 48,   64, 72, 80, 88,  96, 104, 112, 120,   128, 136, 144, //16
+					1,  17, 25, 33, 41, 49, 57, 65, 73, 81, 89, 97, 105,    121,   129, 137, 145,   153, 161, 169, 177, //21
+					2,  18, 26, 34, 42, 50, 58, 66, 74, 82, 90, 98, 106,    122,   130, 138, 146,   154, 162, 170, 178, //21
+					3,    19, 27, 35, 43, 51, 59, 67, 75, 83, 91, 99, 107,   123,                    155, 163, 171, //16
+					4,   12, 20, 28, 36, 44, 52, 60, 68, 76, 84, 92,          124,       140,       156, 164, 172, 180, //17
+					5,  29, 21,      53,            77, 93, 101,  125,   133, 141, 149,   157,    173, //13
+
+					6, 14, 22, 30, 38, 46, 54, 62, 70, 78, 86, 94, 102, 110, 118, 126, 134, 142, 150, 158, 166, 174, 182, 190, 198, 206, 214, 222, 230, 238 //29
+				],
+				vLedNames: [
+					"Esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",         "Print Screen", "Scroll Lock", "Pause Break", //16
+					"`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-_", "=+", "Backspace",                        "Insert", "Home", "Page Up",       "NumLock", "Num /", "Num *", "Num -",  //21
+					"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\",                               "Del", "End", "Page Down",         "Num 7", "Num 8", "Num 9", "Num +",    //21
+					"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "ISO_#", "Enter",                                                      "Num 4", "Num 5", "Num 6",             //16
+					"Left Shift", "ISO_>", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Right Shift",                 "Up Arrow",                       "Num 1", "Num 2", "Num 3", "Num Enter", //17
+					"Left Ctrl", "Left Win", "Left Alt", "Space", "Right Alt", "Fn", "Menu", "Right Ctrl",  "Left Arrow", "Down Arrow", "Right Arrow", "Num 0", "Num .",                       //13
+					"Underglow 1", "Underglow 2", "Underglow 3", "Underglow 4", "Underglow 5", "Underglow 6", "Underglow 7", "Underglow 8", "Underglow 9", "Underglow 10", "Underglow 11", "Underglow 12", //12
+					"Underglow 13", "Underglow 14", "Underglow 15", "Underglow 16", "Underglow 17", "Underglow 18", "Underglow 19", "Underglow 20", "Underglow 21", "Underglow 22", "Underglow 23", "Underglow 24", //12
+					"Underglow 25", "Underglow 26", "Underglow 27", "Underglow 28", "Underglow 29", "Underglow 30" //5
+				],
+				vLedPositions: [
+					[0, 0],    [1, 0], [2, 0], [3, 0], [4, 0],    [6, 0], [7, 0], [8, 0], [9, 0],  [10, 0], [11, 0], [12, 0], [13, 0],      [14, 0], [15, 0], [16, 0],            //20
+					[0, 1],  [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [13, 1],     [14, 1], [15, 1], [16, 1],   [17, 1], [18, 1], [19, 1], [20, 1], //21
+					[0, 2],    [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], [13, 2],   [14, 2], [15, 2], [16, 2],   [17, 2], [18, 2], [19, 2], [20, 3], //20
+					[0, 3],    [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3], [12, 3], [13, 3],                             [17, 3], [18, 3], [19, 3], // 17
+					[0, 4],  [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [11, 4],         [13, 4],           [15, 4],           [17, 4], [18, 4], [19, 4], [20, 4], // 17
+					[0, 5], [1, 5], [2, 5],                      [6, 5],                        [10, 5], [11, 5],  [12, 5], [13, 5],    [14, 5], [15, 5], [16, 5],   [17, 5],         [19, 5],               // 13
+					// underglow
+					[0, 6], [1, 6], [2, 6], [3, 6], [4, 6], [4, 6], [5, 6], [6, 6], [7, 6], [7, 6], [8, 6], [9, 6], [10, 6], [10, 6], [11, 6], [12, 6], [13, 6], [13, 6], [14, 6], [15, 6], [16, 6], [16, 6], [17, 6], [18, 6], [19, 6], [20, 6], [20, 6], [21, 6], [22, 6], [23, 6],
+				],
+				endpoint : { "interface": 1, "usage": 0x0001, "usage_page": 0xFF00, "collection": 0x0000 },
+				LedCount: 136, // Testing purpose
+				LedOffset: 0x87, // Testing purpose
+			},
 			"ROG Strix Scope":
 			{
 				size: [21, 6],
@@ -235,7 +344,6 @@ export class deviceLibrary {
 				endpoint : { "interface": 1, "usage": 0x0001, "usage_page": 0xFF00, "collection": 0x0000 },
 				LedCount: 144, // Testing purpose
 				LedOffset: 0x90, // Testing purpose
-
 			},
 			"ROG Strix Scope TKL":
 			{
@@ -375,6 +483,9 @@ export class deviceLibrary {
 		};
 
 		this.imageLibrary = {
+			"ROG Falchion":			"https://marketplace.signalrgb.com/devices/brands/asus/keyboards/falchion.png",
+			"ROG Strix Flare":		"https://marketplace.signalrgb.com/devices/brands/asus/keyboards/strix-flare-standard.png",
+			"ROG Strix Flare II":	"https://marketplace.signalrgb.com/devices/brands/asus/keyboards/strix-flare-ii-animate.png",
 			"ROG Strix Scope": 		"https://marketplace.signalrgb.com/devices/brands/asus/keyboards/strix-scope-standard.png",
 			"ROG Strix Scope TKL": 	"https://marketplace.signalrgb.com/devices/brands/asus/keyboards/strix-scope-tkl.png",
 			"ROG Strix Scope RX":	"https://marketplace.signalrgb.com/devices/brands/asus/keyboards/strix-scope-rx.png",
