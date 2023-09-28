66b5c08cf4801534c660ba6ef1df7b8d2f5fe19d
Guilherme <guicandido1337@hotmail.com>
2023-09-27 21:03:40 -0300

    Reverting to ANSI

--- Plugins/CoolerMaster/CoolerMaster_CK721_Keyboard.js
+++ Plugins/CoolerMaster/CoolerMaster_CK721_Keyboard.js
@@ -1,11 +1,12 @@
 export function Name() { return "CoolerMaster CK721"; }
 export function VendorId() { return 0x2516; }
-export function ProductId() { return [0x016D, 0x01C3]; } // 0x01C3 Wireless but it doesn't have a "CPU MODE" used to stream RGB data
+export function ProductId() { return [0x016D, 0x01C3]; }
 export function Publisher() { return "WhirlwindFX"; }
 export function Documentation(){ return "troubleshooting/coolermaster"; }
 export function Size() { return [15, 5]; }
 export function DefaultPosition() {return [75, 70]; }
 export function DefaultScale(){return 8.0; }
+export function ConflictingProcesses() { return ["MasterPlusApp.exe"]; }
 /* global
 shutdownColor:readonly
 LightingMode:readonly
@@ -21,24 +22,24 @@ export function ControllableParameters(){
 
 const vLedNames = [
 	"Esc", "1",  "2", "3", "4", "5",  "6", "7", "8", "9", "0",  "-",   "+",  "Backspace", //14
-	"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter",					"Del", //15
-	"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "ISO_#", "\\",					"PgUp", //15
-	"Left Shift", "<", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Right Shift", "Up Arrow", 	"PgDn", //15
+	"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\", "Del", //15
+	"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", 			"Enter", "PgUp", //15
+	"Left Shift", "<", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Right Shift", "Up Arrow", "PgDn", //15
 	"Left Ctrl", "Left Win", "Left Alt", "Space", "Right Alt", "Menu", "Right Ctrl", "Left Arrow",  "Down Arrow", "Right Arrow" //10
 ];
 
 const vLedPositions = [
 	[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0], [12, 0], [13, 0], //14
 	[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [13, 1], [14, 1], //15
-	[0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], [13, 2], [14, 2], //15
+	[0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], 		   [13, 2], [14, 2], //15
 	[0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3], [12, 3], [13, 3],	[14, 3], //15
 	[0, 4], [1, 4], [2, 4],			  		[6, 4],			  				[9, 4], [10, 4], [11, 4], [12, 4], [13, 4], [14, 4] //10
 ];
 
 const vLeds = [
 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, //14
-	15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,	42, 29, //15
-	30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 28,	62,	43, //15
+	15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27,	28, 29, //15
+	30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 	42,	43, //15
 	44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 57,	56, 58, //15
 	59, 60, 61,      	63, 			66, 68, 69, 70, 64, 67	//10
 ];
@@ -52,7 +53,8 @@ export function LedPositions() {
 }
 
 export function Initialize() {
-	device.write([0x00, 0x80, 0x01, 0x01, 0x02, 0x02], 65);
+	device.write([0x00, 0x80, 0x01, 0x01, 0x02, 0x02], 65); //SW Mode?
+	//device.write([0x00, 0x41, 0x02, 0x00, 0x00, 0x00], 65);
 }
 
 export function Render() {
