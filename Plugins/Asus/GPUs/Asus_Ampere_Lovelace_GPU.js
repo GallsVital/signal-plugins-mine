6c24d3b490dc44f37f565176a79683f82382f4f2
Guilherme <guicandido1337@hotmail.com>
2023-10-02 17:40:47 -0300

    4090 Strix Gaming OC second PID

--- Plugins/Asus/GPUs/Asus_Ampere_Lovelace_GPU.js
+++ Plugins/Asus/GPUs/Asus_Ampere_Lovelace_GPU.js
@@ -554,6 +554,7 @@ class Asus_Ampere_Lovelace_IDs {
 		this.RTX4090_STRIX_GAMING                  = 0x889C;
 		this.RTX4090_STRIX_GAMING_2                = 0x889D;
 		this.RTX4090_STRIX_GAMING_OC			   = 0x88EF;
+		this.RTX4090_STRIX_GAMING_OC_2             = 0x8932;
 		this.RTX4090_STRIX_GAMING_WHITE_OC		   = 0x88C3;
 		this.RTX4090_STRIX_GAMING_WHITE			   = 0x88C4;
 		this.RTX4090_STRIX_LC_OC				   = 0x88E8;
@@ -694,6 +695,7 @@ const Asus3000GPUIDs =
 	new AsusGPUIdentifier(Nvidia.RTX4090, AsusID.RTX4090_STRIX_GAMING, "Asus ROG Strix 4090 Gaming OC"),
 	new AsusGPUIdentifier(Nvidia.RTX4090, AsusID.RTX4090_STRIX_GAMING_2, "Asus ROG Strix 4090 Gaming"),
 	new AsusGPUIdentifier(Nvidia.RTX4090, AsusID.RTX4090_STRIX_GAMING_OC, "Asus 4090 ROG Strix Gaming OC"),
+	new AsusGPUIdentifier(Nvidia.RTX4090, AsusID.RTX4090_STRIX_GAMING_OC_2, "Asus 4090 ROG Strix Gaming OC"),
 	new AsusGPUIdentifier(Nvidia.RTX4090, AsusID.RTX4090_STRIX_GAMING_WHITE, "Asus ROG Strix 4090 Gaming White"),
 	new AsusGPUIdentifier(Nvidia.RTX4090, AsusID.RTX4090_STRIX_GAMING_WHITE_OC, "Asus ROG Strix 4090 Gaming White OC"),
 	new AsusGPUIdentifier(Nvidia.RTX4090, AsusID.RTX4090_STRIX_LC_OC, "Asus 4090 ROG Strix LC OC Edition"),
