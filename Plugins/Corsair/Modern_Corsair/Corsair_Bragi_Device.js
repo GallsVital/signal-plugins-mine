fe8ca575f7fff3adb2488cf6b7f504bc38706a5f
TheDordo <ripet1634@gmail.com>
2023-10-05 13:12:26 -0400

    K60 Pro Fixed

--- Plugins/Corsair/Modern_Corsair/Corsair_Bragi_Device.js
+++ Plugins/Corsair/Modern_Corsair/Corsair_Bragi_Device.js
@@ -203,9 +203,10 @@ function setupWiredDevice() {
 		device.setName(wiredDevice.name);
 		device.setSize(wiredDevice.size);
 		device.setControllableLeds(wiredDevice.ledNames, wiredDevice.ledPositions);
+		initializeDevice(wiredDevice);
 	}
 
-	if(devFlags) { createSubdevice(wiredDevice); initializeDevice(wiredDevice); } else { initializeDevice(wiredDevice); }
+	if(devFlags) { createSubdevice(wiredDevice); initializeDevice(wiredDevice); }
 }
 
 /* eslint-disable complexity */
@@ -958,6 +959,7 @@ class CorsairLibrary{
 			0x1bcf : "K65 Mini",
 			0x1bc3 : "K65 Mini",
 			0x1BBD : "K65 Mini",
+			0x1BC0 : "K70 Max",
 			0x1bb6 : "K70 Pro Mini",
 			0x1BC4 : "K70 Pro",
 			0x1BB3 : "K70 Pro",
@@ -990,6 +992,7 @@ class CorsairLibrary{
 			"K65 Mini"  : "https://marketplace.signalrgb.com/devices/default/keyboards/65-keyboard-render.png",
 			"K70 Pro Mini"  : "https://marketplace.signalrgb.com/devices/default/keyboards/keyboard-60.png",
 			"K70 Pro" : "https://marketplace.signalrgb.com/devices/default/keyboards/full-size-keyboard-render.png",
+			"K70 Max" : "https://marketplace.signalrgb.com/devices/default/keyboards/full-size-keyboard-render.png",
 			"K70 TKL" : "https://marketplace.signalrgb.com/devices/default/keyboards/keyboard-80.png",
 			"K95 Plat XT" : "https://marketplace.signalrgb.com/devices/default/keyboards/full-size-keyboard-render.png",
 			"K100 Air" : "https://marketplace.signalrgb.com/devices/default/keyboards/full-size-keyboard-render.png",
@@ -1263,13 +1266,13 @@ class CorsairLibrary{
 					43, 20, 26, 8, 21, 23, 28, 24, 12, 18, 19, 47, 48, 49, 76, 77, 78, 95, 96, 97, 87,
 					57, 4, 22, 7, 9, 10, 11, 13, 14, 15, 51, 52, 40, 92, 93, 94,
 					106, 29, 27, 6, 25, 5, 17, 16, 54, 55, 56, 110, 82, 89, 90, 91, 88,
-					105, 108, 107, 44, 111, 112, 101, 109, 80, 81, 79, 98, 99,
+					105, 108, 107, 44, 111, 122, 101, 109, 80, 81, 79, 98, 99,
 
 					//ISO
 					100, 50
 				 ],
 				devFirmware: "0.0.0",
-				ledSpacing: 109,
+				ledSpacing: 150,
 				keymapType : "Keyboard",
 			},
 			"K65 Mini": { //Broken keymap is very very likely
@@ -1379,7 +1382,48 @@ class CorsairLibrary{
 
 					100, 50 //ISO
 				],
-				devFirmware: "0.0.0",
+				devFirmware: "5.6.126",
+				ledSpacing: 0,
+				keymapType : "Keyboard"
+			},
+			"K70 Max": {
+				name: "K70 Max",
+				size: [22, 7],
+				ledNames: [
+					"Profile", "Brightness", "Lock",    "LeftLogo", "RightLogo",  "VOLUME_MUTE",
+					"Esc", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12",         "Print Screen", "Scroll Lock", "Pause Break", "MediaStop", "MediaRewind", "MediaPlayPause", "MediaFastForward",
+					"`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-_", "=+", "Backspace",      "Insert", "Home", "Page Up",    "NumLock", "Num /", "Num *", "Num -",
+
+					"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\",             "Del", "End", "Page Down",      "Num 7", "Num 8", "Num 9", "Num +",
+					"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter",                                                   "Num 4", "Num 5", "Num 6",
+					"Left Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Right Shift",                          "Up Arrow",           "Num 1", "Num 2", "Num 3", "Num Enter",
+					"Left Ctrl", "Left Win", "Left Alt", "SpaceBar Left", "Space", "SpaceBar Right", "Right Alt", "Fn", "Menu", "Right Ctrl",  "Left Arrow", "Down Arrow", "Right Arrow", "Num 0", "Num .",
+					"ISO #", "ISO <"
+				],
+				ledPositions: [
+					[0, 0], [1, 0], [2, 0],   								    [8, 0], [8, 0],                                                         				  [19, 0],
+					[0, 1],        [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1],         [11, 1], [12, 1], [13, 1], [14, 1], [15, 1], [16, 1], [17, 1], [18, 1], [19, 1], [20, 1], [21, 1],
+					[0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], [13, 2],   	  [15, 2], [16, 2], [17, 2], [18, 2], [19, 2], [20, 2], [21, 2],
+					[0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3], [12, 3], 		  [14, 3], [15, 3], [16, 3], [17, 3], [18, 3], [19, 3], [20, 3], [21, 3],
+					[0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [11, 4],         [13, 4],                                 [18, 4], [19, 4], [20, 4],
+					[0, 5], [1, 5], [2, 5], [3, 5], [4, 5], [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [10, 5],         [12, 5],                         [16, 5],         [18, 5], [19, 5], [20, 5], [21, 5],
+					[0, 6], [1, 6], [2, 6],                      [6, 6], [7, 6], [8, 6],                [11, 6], [12, 6], [13, 6], [14, 6], [15, 6], [16, 6], [17, 6], [18, 6],        [20, 6],
+
+					//ISO
+					[13, 3], [2, 5]
+				],
+				ledMap: [
+					128, 113, 114, 137, 138, 102,
+					41, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 123, 126, 124, 125,
+					53, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 45, 46, 42, 73, 74, 75, 83, 84, 85, 86,
+					43, 20, 26, 8, 21, 23, 28, 24, 12, 18, 19, 47, 48, 49, 76, 77, 78, 95, 96, 97, 87,
+					57, 4, 22, 7, 9, 10, 11, 13, 14, 15, 51, 52, 40, 92, 93, 94,
+					106, 29, 27, 6, 25, 5, 17, 16, 54, 55, 56, 110, 82, 89, 90, 91, 88,
+					105, 108, 107, 140, 44, 141, 111, 122, 101, 109, 80, 81, 79, 98, 99,
+
+					100, 50 //ISO
+				],
+				devFirmware: "5.6.126",
 				ledSpacing: 0,
 				keymapType : "Keyboard"
 			},
