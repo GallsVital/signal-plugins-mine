5e5612b777fd4d8e98816f32fb670f714f8064c1
Guilherme <guicandido1337@hotmail.com>
2023-11-03 13:47:11 -0300

    LEDs layout fixed

--- Plugins/Razer/Razer_Modern_Keyboard.js
+++ Plugins/Razer/Razer_Modern_Keyboard.js
@@ -132,11 +132,7 @@ function detectInputs() {
 }
 
 function spawnMacroHelpers() {
-	if(Razer.getDeviceType() === "Keyboard") {
-		device.addFeature("keyboard");
-	} else {
-		device.addFeature("mouse");
-	}
+	device.addFeature("keyboard");
 }
 
 function processInputs(Added, Removed) {
@@ -144,21 +140,15 @@ function processInputs(Added, Removed) {
 	for (let values = 0; values < Added.length; values++) {
 		const input = Added.pop();
 
-		if(Razer.getDeviceType() === "Keyboard") {
-			processKeyboardInputs(input);
-		} else {
-			processMouseInputs(input);
-		}
+		processKeyboardInputs(input);
+
 	}
 
 	for (let values = 0; values < Removed.length; values++) {
 		const input = Removed.pop();
 
-		if(Razer.getDeviceType() === "Keyboard") {
-			processKeyboardInputs(input, true);
-		} else {
-			processMouseInputs(input, true);
-		}
+		processKeyboardInputs(input, true);
+
 	}
 }
 
@@ -170,45 +160,6 @@ function processKeyboardInputs(input, released = false) {
 	const eventData = { key : Razer.getInputDict()[input], keyCode : 0, "released": released };
 	device.log(`${Razer.getInputDict()[input]} Hit. Release Status: ${released}`);
 	keyboard.sendEvent(eventData, "Key Press");
-}
-
-function processMouseInputs(input, released = false) {
-	if(released) {
-		if(input === 0x51) {
-			device.log("DPI Clutch Released.");
-			DpiHandler.SetSniperMode(false);
-		} else {
-			const eventData = { "buttonCode": 0, "released": true, "name": Razer.getInputDict()[input] };
-			device.log(Razer.getInputDict()[input] + " released.");
-			mouse.sendEvent(eventData, "Button Press");
-		}
-
-		return;
-	}
-
-	switch (input) {
-	case 0x20:
-		device.log("DPI Up");
-		DpiHandler.increment();
-		break;
-	case 0x21:
-		device.log("DPI Down");
-		DpiHandler.decrement();
-		break;
-
-	case 0x51:
-		device.log("DPI Clutch Hit.");
-		DpiHandler.SetSniperMode(true);
-		break;
-	case 0x52:
-		device.log("DPI Cycle Hit.");
-		DpiHandler.increment();
-		break;
-	default:
-		const eventData = { "buttonCode": 0, "released": false, "name": Razer.getInputDict()[input] };
-		device.log(Razer.getInputDict()[input] + " hit.");
-		mouse.sendEvent(eventData, "Button Press");
-	}
 }
 
 function grabLighting(overrideColor) {
@@ -255,17 +206,6 @@ function hexToRgb(hex) {
 
 export class deviceLibrary {
 	constructor() {
-
-		this.mouseInputDict = {
-			0x20 : "DPI Up",
-			0x21 : "DPI Down",
-			0x22 : "Right Back Button",
-			0x23 : "Right Forward Button",
-			0x50 : "Profile Button",
-			0x51 : "DPI Clutch",
-			0x52 : "DPI Cycle",
-			0x54 : "Scroll Accel Button"
-		};
 
 		this.keyboardInputDict = {
 			0x20 : "M1",
@@ -328,7 +268,7 @@ export class deviceLibrary {
 				endpoint : { "interface": 3, "usage": 0x0001, "usage_page": 0x000C },
 				DeviceType : "Keyboard",
 				ledsToSend : 15,
-				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/blackwidow-v4-75.png"
+				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/blackwidow-v3-mini.png"
 			},
 			"Blackwidow V4" :
 			{
@@ -394,7 +334,7 @@ export class deviceLibrary {
 				endpoint : { "interface": 3, "usage": 0x0000, "usage_page": 0x0001 },
 				DeviceType : "Keyboard",
 				ledsToSend : 23,
-				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/blackwidow-v4-pro.png"
+				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/blackwidow-v4.png"
 			},
 			"Blackwidow V4 X" :
 			{
@@ -451,7 +391,7 @@ export class deviceLibrary {
 				endpoint : { "interface": 3, "usage": 0x0000, "usage_page": 0x0001 },
 				DeviceType : "Keyboard",
 				ledsToSend : 23,
-				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/blackwidow-v4-pro.png"
+				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/blackwidow-v4-x.png"
 			},
 			"Blackwidow V4 Pro" :
 			{
@@ -465,11 +405,11 @@ export class deviceLibrary {
 					142, 70,  71,  72,  73,  74,  75,  76,  77,  78,  79,  80,  81,  82,       84,					88,  89,  90, 		    151,
 					143, 93,  94,  96,  97,  98,  99,  100, 101, 102, 103, 104, 105, 107,     			 109,		111, 112, 113, 114,     150,
 					144, 116, 117, 118, 119,                123,                127, 128, 129, 130, 131, 132, 133,	135,	  136,			149,
-					145,																						  							148,
-					146,																					      							147,
-					161,																						  							180,
-					162,																													179,
-					163,																						  							178,
+					145, 148,
+					146, 147,
+					161, 180,
+					162, 179,
+					163, 178,
 					164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177
 				],
 
@@ -482,11 +422,11 @@ export class deviceLibrary {
 					"Underglow Left LED 5", "M3", "CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter",                                                  		          "Num 4", "Num 5", "Num 6",               "Underglow Right LED 5", //19
 					"Underglow Left LED 6", "M2", "Left Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Right Shift",                   	   "Up Arrow",                            "Num 1", "Num 2", "Num 3", "Num Enter",  "Underglow Right LED 6", //20
 					"Underglow Left LED 7", "M1", "Left Ctrl", "Left Win", "Left Alt", "Space", "Right Alt", "Fn", "Menu", "Right Ctrl",      "Left Arrow",  "Down Arrow", "Right Arrow",     "Num 0", "Num .",                        "Underglow Right LED 7", //16
-					"Underglow Left LED 8",                                                                                                                                                                                            "Underglow Right LED 8",
-					"Underglow Left LED 9",                                                                                                                                                                                            "Underglow Right LED 9",
-					"Underglow Left LED 10",                                                                                                                                                                                           "Underglow Right LED 10",
-					"Underglow Left LED 11",                                                                                                                                                                                           "Underglow Right LED 11",
-					"Underglow Left LED 12",                                                                                                                                                                                           "Underglow Right LED 12",
+					"Underglow Left LED 8", "Underglow Right LED 8",
+					"Underglow Left LED 9", "Underglow Right LED 9",
+					"Underglow Left LED 10", "Underglow Right LED 10",
+					"Underglow Left LED 11", "Underglow Right LED 11",
+					"Underglow Left LED 12", "Underglow Right LED 12",
 					"Underglow Left LED 13",  "Underglow Bottom 1", "Underglow Bottom 2", "Underglow Bottom 3", "Underglow Bottom 4", "Underglow Bottom 5", "Underglow Bottom 6", "Underglow Bottom 7", "Underglow Bottom 8", "Underglow Bottom 9", "Underglow Bottom 10", "Underglow Bottom 11", "Underglow Bottom 12", "Underglow Right LED 13",
 				],
 
@@ -499,12 +439,12 @@ export class deviceLibrary {
 					[0, 4], [1, 4], [2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [11, 4], [12, 4], [13, 4], [14, 4],                           		              [20, 4], [21, 4], [22, 4],		  [24, 4], //19
 					[0, 5], [1, 5], [2, 5], 		[4, 5], [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [10, 5], [11, 5], [12, 5], [13, 5], [14, 5],  		            		[18, 5],          [20, 5], [21, 5], [22, 5], [23, 5], [24, 5], //19
 					[0, 6], [1, 6], [2, 6], [3, 6], [4, 6],                                 [9, 6],                            [13, 6], [14, 6], [15, 6], [16, 6], [17, 6], [18, 6], [19, 6], [20, 6], 			[22, 6],		  [24, 6],
-					[0, 7],																																																		  [24, 7],
-					[0, 8],																																																		  [24, 8],
-					[0, 9],																																																		  [24, 9],
-					[0, 10],																																																	  [24, 10],
-					[0, 11],																																																	  [24, 11],
-					[0, 12],     [1, 12],        [3, 12],       [5, 12],        [7, 12],       [9, 12],        [11, 12],        [13, 12],        [15, 12],		[17, 12], 	[19, 12], [21, 12], [23, 12],						  [24, 12]
+					[0, 7], [24, 7],
+					[0, 8], [24, 8],
+					[0, 9], [24, 9],
+					[0, 10], [24, 10],
+					[0, 11], [24, 11],
+					[0, 12], [1, 12], [3, 12], [5, 12], [7, 12], [9, 12], [11, 12], [13, 12], [15, 12], [17, 12], [19, 12], [21, 12], [23, 12], [24, 12]
 
 				],
 				endpoint : { "interface": 3, "usage": 0x0000, "usage_page": 0x0001 },
@@ -580,7 +520,7 @@ export class deviceLibrary {
 			},
 			"Huntsman V3 Pro" :
 			{
-				size : [25, 9],
+				size : [21, 6],
 				vKeys :
 				[
 					2,   4,   5,   6,   7,   8,  9,   10,  11,  12,  13,  14,  15,       16,  17,  18,	19,  20,  21,  22,	//21
@@ -601,17 +541,17 @@ export class deviceLibrary {
 				],
 				vLedPositions :
 				[
-					[2, 1],			[4, 1], [5, 1], [6, 1], [7, 1],			[9, 1], [10, 1], [11, 1], [12, 1], [13, 1], [14, 1], [15, 1], [16, 1], [17, 1], [18, 1], [19, 1], [20, 1], [21, 1], [22, 1], [23, 1],	//21
-					[2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], [13, 2], [14, 2], [15, 2], 		   [17, 2], [18, 2], [19, 2], [20, 2], [21, 2], [22, 2], [23, 2],	//22
-					[2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3], [12, 3], [13, 3], [14, 3], [15, 3], 		   [17, 3], [18, 3], [19, 3], [20, 3], [21, 3], [22, 3], [23, 3],	//22
-					[2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [11, 4], [12, 4], [13, 4], [14, 4],                           		              [20, 4], [21, 4], [22, 4],			//17
-					[2, 5], 		[4, 5], [5, 5], [6, 5], [7, 5], [8, 5], [9, 5], [10, 5], [11, 5], [12, 5], [13, 5], [14, 5],  		            		[18, 5],          [20, 5], [21, 5], [22, 5], [23, 5],	//18
-					[2, 6], [3, 6], [4, 6],                                 [9, 6],                            [13, 6], [14, 6], [15, 6], [16, 6], [17, 6], [18, 6], [19, 6], [20, 6], 			[22, 6],			//14
+					[0, 0], 		[2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0], [12, 0], [13, 0],   [14, 0], 			[16, 0],	[17, 0], [18, 0], [19, 0], [20, 0],	//21
+					[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [13, 1],   [14, 1], [15, 1], [16, 1],	[17, 1], [18, 1], [19, 1], [20, 1],	//22
+					[0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2], [13, 2],   [14, 2], [15, 2], [16, 2],	[17, 2], [18, 2], [19, 2], [20, 2],	//22
+					[0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3], 		   [13, 3],									[17, 3], [18, 3], [19, 3],			//17
+					[0, 4], 		[2, 4], [3, 4], [4, 4], [5, 4], [6, 4], [7, 4], [8, 4], [9, 4], [10, 4], [11, 4],          [13, 4],            [15, 4],				[17, 4], [18, 4], [19, 4], [20, 4],	//18
+					[0, 5], [1, 5], [2, 5],                 		[6, 5],                       	[10, 5], [11, 5], [12, 5], [13, 5],   [14, 5], [15, 5], [16, 5],	[17, 5],		  [19, 5],			//14
 				],
 				endpoint : { "interface": 3, "usage": 0x0000, "usage_page": 0x0001 },
 				DeviceType : "Keyboard",
 				ledsToSend : 23,
-				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/blackwidow-v4-pro.png"
+				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/huntsman-v3-pro.png"
 			},
 			"Huntsman V3 Pro TKL" :
 			{
@@ -646,11 +586,11 @@ export class deviceLibrary {
 				endpoint : { "interface": 3, "usage": 0x0001, "usage_page": 0x000C },
 				DeviceType : "Keyboard",
 				ledsToSend : 21,
-				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/blackwidow-v4-75.png"
+				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/huntsman-v3-pro-tkl.png"
 			},
 			"Huntsman V3 Pro Mini" :
 			{
-				size : [15, 6],
+				size : [14, 5],
 				vKeys :
 				[
 					0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 14,
@@ -680,7 +620,7 @@ export class deviceLibrary {
 				endpoint : { "interface": 3, "usage": 0x0001, "usage_page": 0x000C },
 				DeviceType : "Keyboard",
 				ledsToSend : 15,
-				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/blackwidow-v4-75.png"
+				image: "https://marketplace.signalrgb.com/devices/brands/razer/keyboards/huntsman-v3-pro-mini.png"
 			},
 		};
 	}
@@ -875,21 +815,6 @@ export class RazerProtocol {
 				this.setDeviceType(layout.DeviceType);
 			}
 
-			if(layout.maxDPI) {
-				device.log(`Max DPI: ${layout.maxDPI}`);
-				RazerMouse.setMaxDPI(layout.maxDPI);
-			}
-
-			if(layout.hasSniperButton) {
-				device.log("Device has Sniper Button.");
-				RazerMouse.setHasSniperButton(layout.hasSniperButton);
-			}
-
-			if(layout.requiresApplyPacket) {
-				device.log("Device Requires Apply Packet");
-				this.setRequiresApplyPacket(layout.requiresApplyPacket);
-			}
-
 		} else {
 			device.log("No Valid Library Config found.");
 		}
@@ -900,11 +825,7 @@ export class RazerProtocol {
 		if (layout.hyperflux) { this.setHyperFluxProperties(); }
 	}
 	setDeviceMacroProperties() {
-		if (this.getDeviceType() === "Keyboard") {
-			this.setInputDict(razerDeviceLibrary.keyboardInputDict);
-		} else {
-			this.setInputDict(razerDeviceLibrary.mouseInputDict);
-		}
+		this.setInputDict(razerDeviceLibrary.keyboardInputDict);
 	}
 	setHyperFluxProperties() {
 		device.log("Device has a Hyperflux Pad!");
