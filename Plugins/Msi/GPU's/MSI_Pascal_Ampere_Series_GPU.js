8c773d32971b9854837a0efbfaaef38ef2591ee8
TheDordo <ripet1634@gmail.com>
2023-11-21 16:20:22 -0500

    Add MSI GPU Warnings to help with common questions

--- Plugins/Msi/GPU's/MSI_Pascal_Ampere_Series_GPU.js
+++ Plugins/Msi/GPU's/MSI_Pascal_Ampere_Series_GPU.js
@@ -20,6 +20,12 @@ export function ControllableParameters() {
 	];
 }
 
+export function DeviceMessages() {
+	return [
+		{property: "Single Zone Control", message:"GPU is Limited to a Single Zone.", tooltip: "This device's firmware is limited to a single rgb zone while controlled directly."},
+	];
+}
+
 const vLedNames = [ "GPU" ];
 const vLedPositions = [ [3, 1]];
 
