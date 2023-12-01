48d60ca6b289ce2456483e660c5f05e7468e4dd4
Guilherme <guicandido1337@hotmail.com>
2023-12-01 13:08:08 -0300

    Huntsman Mini merged

--- Plugins/Razer/Razer_Modern_Keyboard.js
+++ Plugins/Razer/Razer_Modern_Keyboard.js
@@ -224,6 +224,8 @@ export class deviceLibrary {
 			0x0293 : "Blackwidow V4 X",
 			0x028D : "Blackwidow V4 Pro",
 			0x02A5 : "Blackwidow V4 75%",
+			0x0257 : "Huntsman Mini",
+			0x005E : "Huntsman Mini",
 			0x0266 : "Huntsman V2 Analog",
 			//0x0000 : "Huntsman V3 Pro",
 			0x02A7 : "Huntsman V3 Pro TKL",
@@ -567,6 +569,40 @@ export class deviceLibrary {
 				DeviceType : "Keyboard",
 				LEDsPerPacket : 18,
 				image: "https://assets.signalrgb.com/devices/brands/razer/keyboards/blackwidow-v4-75.png"
+			},
+			"Huntsman Mini" :
+			{
+				size : [15, 6],
+				vKeys :
+				[
+					0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10, 11, 12, 14,
+					16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+					32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 45,
+					48,     50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 61,
+					64, 65, 66,             70,         74, 75, 76,
+				],
+
+				vLedNames :
+				[
+					"Esc", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-_", "=+", "Backspace",
+					"Tab", "Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "[", "]", "\\",
+					"CapsLock", "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "Enter",
+					"Left Shift", "Z", "X", "C", "V", "B", "N", "M", ",", ".", "/", "Right Shift",
+					"Left Ctrl", "Left Win", "Left Alt", "Space", "Right Alt", "Fn", "Right Ctrl",
+				],
+
+				vLedPositions :
+				[
+					[0, 0], [1, 0], [2, 0], [3, 0], [4, 0], [5, 0], [6, 0], [7, 0], [8, 0], [9, 0], [10, 0], [11, 0], [12, 0], [13, 0],           //15
+					[0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1], [10, 1], [11, 1], [12, 1], [13, 1],           //15
+					[0, 2], [1, 2], [2, 2], [3, 2], [4, 2], [5, 2], [6, 2], [7, 2], [8, 2], [9, 2], [10, 2], [11, 2], [12, 2],            //14
+					[0, 3], [1, 3], [2, 3], [3, 3], [4, 3], [5, 3], [6, 3], [7, 3], [8, 3], [9, 3], [10, 3], [11, 3],                    //14
+					[0, 4], [1, 4], [2, 4],                         [6, 4],                 [9, 4], [10, 4], [11, 4],           //10
+				],
+				endpoint : { "interface": 2, "usage": 0x0002, "usage_page": 0x0001 },
+				DeviceType : "Keyboard",
+				LEDsPerPacket : 15,
+				image: "https://assets.signalrgb.com/devices/brands/razer/keyboards/huntsman-mini.png"
 			},
 			"Huntsman V2 Analog" :
 			{
