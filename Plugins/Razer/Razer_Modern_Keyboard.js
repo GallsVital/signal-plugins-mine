1583fc06dd3ded4b761375aa55c0a5c14b88ef65
Guilherme <guicandido1337@hotmail.com>
2023-12-04 20:34:06 -0300

    input id log

--- Plugins/Razer/Razer_Modern_Keyboard.js
+++ Plugins/Razer/Razer_Modern_Keyboard.js
@@ -157,6 +157,7 @@ function processKeyboardInputs(input, released = false) {
 	}
 
 	const eventData = { key : Razer.getInputDict()[input], keyCode : 0, "released": released };
+	device.log(input);
 	device.log(`${Razer.getInputDict()[input]} Hit. Release Status: ${released}`);
 	keyboard.sendEvent(eventData, "Key Press");
 }
@@ -754,12 +755,12 @@ export class deviceLibrary {
 				size : [17, 6],
 				vKeys :
 				[
-					1,   3,   4,   5,   6,   7,   8,   9,  10,  11,  12,  13,  14,       15,       18,
-					20,  21,  22,  23,  24,  25,  26,  27,  28,  29,  30,  31,  32,  33,  34,  35,  36,
-					39,  40,  41,  42,  43,  44,  45,  46,  47,  48,  49,  50,  51,  52,  53,  54,  55,
-					58,  59,  60,  61,  62,  63,  64,  65,  66,  67,  68,  69,       71,
-					77,  79,  80,  81,  82,  83,  84,  85,  86,  87,  88,  90,                92,
-					96, 97, 98,                102,                106, 107, 108, 109,  110, 111, 112,
+					0,   0,   0,   0,   0,   0,   0,   0,  0,  0,  0,  0,  0,       0,       0,
+					0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+					0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+					0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,       0,
+					0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,                0,
+					0, 0, 0,                0,                0, 0, 0, 0,  0, 0, 0,
 				],
 				vLedNames :
 				[
