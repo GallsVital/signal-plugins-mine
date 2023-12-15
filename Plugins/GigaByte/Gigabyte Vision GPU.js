5f61ea761358a66abb61ab65d3b7303424d0e280
Guilherme <guicandido1337@hotmail.com>
2023-12-14 23:23:56 -0300

    Changing SKU

--- Plugins/GigaByte/Gigabyte Vision GPU.js
+++ Plugins/GigaByte/Gigabyte Vision GPU.js
@@ -373,8 +373,8 @@ class GigabyteVisionDeviceIds {
 
 		this.RTX4060TI_GAMING_OC            = 0x40F8;
 		this.RTX4060TI_GAMING_OC_2			= 0x4112;
+		this.RTX4060TI_AERO					= 0x40F9;
 		this.RTX4060TI_AERO_OC				= 0x4113;
-		this.RTX4060TI_AERO_OC_2			= 0x40F9;
 		this.RTX4070_AERO					= 0x40E6;
 		this.RTX4070_EAGLE_OC				= 0x40ED;
 		this.RTX4070_WINDFORCE_OC			= 0x40EE;
@@ -526,11 +526,10 @@ class GigabyteVisionGPuList {
 			new GigabyteVisionIdentifier(Nvidia.RTX3090,        GigabyteVisionIds.RTX3090_GAMING_OC_24GB,		0x62, "GIGABYTE 3090 Gaming OC 24G"), // Confirmed
 
 			new GigabyteVisionIdentifier(Nvidia.RTX4060TI,		GigabyteVisionIds.RTX4060TI_GAMING_OC,			0x71, "GIGABYTE 4060Ti Gaming OC"),
+			new GigabyteVisionIdentifier(Nvidia.RTX4060TI,		GigabyteVisionIds.RTX4060TI_AERO,				0x71, "GIGABYTE 4060Ti Aero OC"),
 
 			new GigabyteVisionIdentifier(Nvidia.RTX4060TI_OC,	GigabyteVisionIds.RTX4060TI_GAMING_OC_2,		0x71, "GIGABYTE 4060Ti Gaming OC"),
 			new GigabyteVisionIdentifier(Nvidia.RTX4060TI_OC,	GigabyteVisionIds.RTX4060TI_AERO_OC,			0x71, "GIGABYTE 4060Ti Aero OC"),
-			new GigabyteVisionIdentifier(Nvidia.RTX4060TI_OC,	GigabyteVisionIds.RTX4060TI_AERO_OC_2,			0x71, "GIGABYTE 4060Ti Aero OC"),
-
 
 			new GigabyteVisionIdentifier(Nvidia.RTX4070,		GigabyteVisionIds.RTX4070_AERO,					0x71, "GIGABYTE 4070 Aero"),
 			new GigabyteVisionIdentifier(Nvidia.RTX4070,		GigabyteVisionIds.RTX4070_EAGLE_OC,				0x71, "GIGABYTE 4070 Eagle OC"),
