4f7564233db2e30be7e3c5ae5bcc208c51f6d314
TheDordo <ripet1634@gmail.com>
2024-01-23 13:25:38 -0500

    Unify the Function keyboard plugins and convert them to use the less terrible protocol (That still flickers)

--- Plugins/Nzxt/Peripheral Protocol/NZXT_Function_Keyboard.js
+++ Plugins/Nzxt/Peripheral Protocol/NZXT_Function_Keyboard.js
@@ -1,11 +1,11 @@
-export function Name() { return "NZXT Function"; }
+export function Name() { return "NZXT Keyboard"; }
 export function VendorId() { return 0x1E71; }
-export function ProductId() { return [0x2103, 0x2106]; }
-export function Publisher() { return "WhirlwindFX"; }
+export function ProductId() { return Object.keys(NZXTdeviceLibrary.PIDLibrary); }
+export function Publisher() { return "WhirlwindFx"; }
 export function Documentation(){ return "troubleshooting/nzxt"; }
-export function Size() { return [10, 10]; }
-export function DefaultPosition(){return [240, 120];}
-export function DefaultScale(){return 8.0;}
+export function Size() { return [1, 1]; }
+export function DefaultPosition(){return [0, 0];}
+export function DefaultScale(){return 1.0;}
 /* global
 shutdownColor:readonly
 LightingMode:readonly
@@ -19,292 +19,165 @@ export function ControllableParameters() {
 	];
 }
 
-const vLedNames = [ "Zone 1", "Zone 2", "Zone 3", "Zone 4", "Zone 5", "Zone 6", "Zone 7", "Zone 8", "Zone 9", "Zone 10" ];
-
-const vLedPositions = [ [0, 1], [1, 1], [3, 1], [4, 1], [9, 1], [8, 1], [7, 1], [6, 1], [5, 1], [2, 1] ];
-
-export function LedNames() {
-	return vLedNames;
-}
-
-export function LedPositions() {
-	return vLedPositions;
-}
-
 export function Initialize() {
-	Header();
-	Header2();
+	NZXT.InitializeNZXT();
 }
 
 export function Render() {
-	sendColorBurstOne();
-	sendColorBurstTwo();
-	sendColorBurstThree();
-	sendColorBurstFour();
+	NZXT.sendColors();
 }
 
 export function Shutdown(SystemSuspending) {
 
 	if(SystemSuspending){
-		sendColorBurstOne("#000000"); // Go Dark on System Sleep/Shutdown
-		sendColorBurstTwo("#000000");
-		sendColorBurstThree("#000000");
-		sendColorBurstFour("#000000");
+		NZXT.sendColors("#000000"); // Go Dark on System Sleep/Shutdown
 	}else{
-		sendColorBurstOne(shutdownColor);
-		sendColorBurstTwo(shutdownColor);
-		sendColorBurstThree(shutdownColor);
-		sendColorBurstFour(shutdownColor);
+		NZXT.sendColors(shutdownColor);
 	}
 
 }
 
-function Header() {
-	const packet = [0x43, 0x81, 0x00, 0x84];
-	device.write(packet, 10);
-}
+export class NZXT_Keyboard_Protocol {
+	constructor() {
+		this.Config = {
+			DeviceProductID: 0x0000,
+			DeviceName: "NZXT Keyboard",
+			DeviceEndpoint: { "interface": 1, "usage": 0x0001, "usage_page": 0xFF01, "collection": 0x0000 },
+			LedNames: [],
+			LedPositions: [],
+			lastRGBData : new Array(30, 0x00)
+		};
+	}
 
-function Header2() {
-	const packet = [0x43, 0x81, 0x00, 0x86];
-	device.write(packet, 10);
-}
+	getDeviceProperties(deviceName) { return NZXTdeviceLibrary.LEDLibrary[deviceName];};
 
-function sendColorBurstOne(overrideColor) {
-	const packet = [];
+	getDeviceProductId() { return this.Config.DeviceProductID; }
+	setDeviceProductId(productID) { this.Config.DeviceProductID = productID; }
 
-	for(let zone_idx = 0; zone_idx < 2; zone_idx++) {
-		const offset = zone_idx;
-		const iX = vLedPositions[offset][0];
-		const iY = vLedPositions[offset][1];
-		let col;
+	getDeviceName() { return this.Config.DeviceName; }
+	setDeviceName(deviceName) { this.Config.DeviceName = deviceName; }
 
-		if(overrideColor) {
-			col = hexToRgb(overrideColor);
-		} else if (LightingMode === "Forced") {
-			col = hexToRgb(forcedColor);
-		} else {
-			col = device.color(iX, iY);
+	getDeviceEndpoint() { return this.Config.DeviceEndpoint; }
+	setDeviceEndpoint(deviceEndpoint) { this.Config.DeviceEndpoint = deviceEndpoint; }
+
+	getLedNames() { return this.Config.LedNames; }
+	setLedNames(ledNames) { this.Config.LedNames = ledNames; }
+
+	getLedPositions() { return this.Config.LedPositions; }
+	setLedPositions(ledPositions) { this.Config.LedPositions = ledPositions; }
+
+	getDeviceImage(deviceName) { return NZXTdeviceLibrary.imageLibrary[deviceName]; }
+
+	InitializeNZXT() {
+		//Initializing vars
+		this.setDeviceProductId(device.productId());
+		this.setDeviceName(NZXTdeviceLibrary.PIDLibrary[this.getDeviceProductId()]);
+
+		const DeviceProperties = this.getDeviceProperties(this.getDeviceName());
+		this.setDeviceEndpoint(DeviceProperties.endpoint);
+		this.setLedNames(DeviceProperties.vLedNames);
+		this.setLedPositions(DeviceProperties.vLedPositions);
+
+		device.log(`Device model found: ` + this.getDeviceName());
+		device.setName("NZXT " + this.getDeviceName());
+		device.setSize(DeviceProperties.size);
+		device.setControllableLeds(this.getLedNames(), this.getLedPositions());
+		device.setImageFromUrl(this.getDeviceImage(this.getDeviceName()));
+		device.set_endpoint(DeviceProperties.endpoint[`interface`], DeviceProperties.endpoint[`usage`], DeviceProperties.endpoint[`usage_page`], DeviceProperties.endpoint[`collection`]);
+		this.setSoftwareMode();
+	}
+
+	setSoftwareMode() {
+		device.write([0x43, 0x81, 0x00, 0x84], 64);
+		device.write([0x43, 0x81, 0x00, 0x86], 64);
+		device.write([0x43, 0x97, 0x00, 0x10, 0x01], 64);
+		device.clearReadBuffer();
+	}
+
+	sendColors(overrideColor) {
+		const deviceLedPositions = this.getLedPositions();
+
+		const packet = [0x43, 0xbd, 0x01, 0x10, 0x02, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0x00, 0x0a]; //This protocol is less stupid than the previous iteration. Current iteration uses a single packet. the 0xff's are setting every key in every bank on. 0x0a is the number of zones.
+		const RGBData = [];
+
+		for(let idx = 0; idx < deviceLedPositions.length; idx++) {
+			const iPxX = deviceLedPositions[idx][0];
+			const iPxY = deviceLedPositions[idx][1];
+			let col;
+
+			if(overrideColor) {
+				col = hexToRgb(overrideColor);
+			} else if (LightingMode === "Forced") {
+				col = hexToRgb(forcedColor);
+			} else {
+				col = device.color(iPxX, iPxY);
+			}
+
+			RGBData[idx * 4] = col[0];
+			RGBData[idx * 4 + 1] = col[1];
+			RGBData[idx * 4 + 2] = col[2];
+			RGBData[idx * 4 + 3] = 0;
 		}
-
-		packet[(zone_idx * 22) + 23] = col[0];
-		packet[(zone_idx * 22) + 24] = col[1];
-		packet[(zone_idx * 22) + 25] = col[2];
+	
+		if(!CompareArrays(this.Config.lastRGBData, RGBData)) {
+			this.Config.lastRGBData = RGBData;
+			device.write(packet.concat(RGBData),64);
+			device.write([0x43, 0x01], 64);
+		}
+	
+		device.clearReadBuffer();
 	}
-
-	packet[0x00]   = 0x43;
-	packet[0x01]   = 0xbd;
-	packet[0x02]   = 0x03;
-	packet[0x03]   = 0x10;
-	packet[0x04]   = 0x01;
-	packet[0x05] = 0x01; //Mapping Esc Key
-	packet[0x06] = 0x00; //Zero Mapping of Second Bank
-	packet[0x07] = 0x03;
-	packet[0x08] = 0x00;
-	packet[0x09] = 0x03;
-	packet[0x0A] = 0x00;
-	packet[0x0B] = 0x03;
-	packet[0x0C] = 0x00;
-	packet[0x0D] = 0x01;
-	packet[0x0E] = 0x00;
-	packet[0x0F] = 0x03;
-	packet[0x10] = 0x00;
-	packet[0x11] = 0x00;
-	packet[0x12] = 0x00;
-	packet[0x13] = 0x00;
-	packet[0x14] = 0x00;
-	packet[0x1A] = 0x01;
-	packet[0x1B] = 0x06;
-	packet[29] = 0x0C;
-	packet[31] = 0x0C;
-	packet[33] = 0x0C;
-	packet[35] = 0x0C;
-	packet[37] = 0x04;
-	packet[48] = 0x01;
-	packet[49] = 0x60;
-	packet[51] = 0xC0;
-	packet[53] = 0xC0;
-	packet[55] = 0xC0;
-	packet[57] = 0xC0;
-	packet[59] = 0x20;
-
-	device.write(packet, 65);
 }
 
-function sendColorBurstTwo(overrideColor) {
+export class deviceLibrary {
+	constructor(){
+		this.PIDLibrary	=	{
+			0x2103: "Function",
+			0x2106: "Function", // ISO
+			0x2104: "Function TKL",
+			0x2107: "Function TKL", // ISO
+			0x2105: "Function MiniTKL",
+			0x2108: "Function MiniTKL", // ISO
+		};
 
-	const packet = [];
+		this.LEDLibrary	=	{
+			"Function":
+			{
+				size: [10, 5],
+				vLedNames: [ "Zone 1", "Zone 2", "Zone 3", "Zone 4", "Zone 5", "Zone 6", "Zone 7", "Zone 8", "Zone 9", "Zone 10" ],
+				vLedPositions: [ [0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1] ],
+				endpoint : { "interface": 1, "usage": 0x0001, "usage_page": 0xFFCA, "collection": 0x0000 },
+			},
+			"Function TKL":
+			{
+				size: [10, 5],
+				vLedNames: [ "Zone 1", "Zone 2", "Zone 3", "Zone 4", "Zone 5", "Zone 6", "Zone 7", "Zone 8", "Zone 9", "Zone 10" ],
+				vLedPositions: [ [0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1] ],
+				endpoint : { "interface": 1, "usage": 0x0001, "usage_page": 0xFFCA, "collection": 0x0000 },
+			},
+			"Function MiniTKL":
+			{
+				size: [10, 5],
+				vLedNames: [ "Zone 1", "Zone 2", "Zone 3", "Zone 4", "Zone 5", "Zone 6", "Zone 7", "Zone 8", "Zone 9", "Zone 10" ],
+				vLedPositions: [ [0, 1], [1, 1], [2, 1], [3, 1], [4, 1], [5, 1], [6, 1], [7, 1], [8, 1], [9, 1] ],
+				endpoint : { "interface": 1, "usage": 0x0001, "usage_page": 0xFFCA, "collection": 0x0000 },
+			},
+		};
 
-	for(let zone_idx = 0; zone_idx < 3; zone_idx++) {
-		const offset = zone_idx + 2;
-		const iX = vLedPositions[offset][0];
-		const iY = vLedPositions[offset][1];
-		let col;
-
-		if(overrideColor) {
-			col = hexToRgb(overrideColor);
-		} else if (LightingMode == "Forced") {
-			col = hexToRgb(forcedColor);
-		} else {
-			col = device.color(iX, iY);
-		}
-
-		packet[(zone_idx * 22) + 6] = col[0];
-		packet[(zone_idx * 22) + 7] = col[1];
-		packet[(zone_idx * 22) + 8] = col[2];
+		this.imageLibrary = {
+			"Function": 		"https://marketplace.signalrgb.com/devices/brands/nzxt/keyboards/function.png",
+			"Function TKL": 	"https://marketplace.signalrgb.com/devices/brands/nzxt/keyboards/function-tkl.png",
+			"Function MiniTKL":	"https://marketplace.signalrgb.com/devices/brands/nzxt/keyboards/function-mini-tkl.png",
+		};
 	}
-
-	packet[0x00]   = 0x43;
-	packet[0x01]   = 0x3d;
-	packet[0x02]   = 0x02;
-	packet[0x03]   = 0x00;
-	packet[0x04]   = 0x00;
-
-	packet[0x05] = 0x00;
-
-	packet[0x09] = 0x01; //Activation Byte
-	packet[0x0A] = 0x80; //Binary Mapping
-
-	packet[0x0B] = 0x01;
-	packet[0x0C] = 0x00;
-	packet[0x0D] = 0x03;
-	packet[0x0E] = 0x00;
-	packet[0x0F] = 0x03;
-	packet[0x10] = 0x00;
-	packet[0x11] = 0x03;
-	packet[0x12] = 0x00;
-	packet[0x13] = 0x03;
-	packet[0x14] = 0x00;
-
-	packet[0x1B] = 0x00; //Blank as we only dropped one byte
-
-	packet[0x1F] = 0x01;//Activation Byte
-	packet[0x2A] = 0x00;//Binary Mapping We drop a byte and lose half our banks.
-	packet[0x2c] = 0xC0;
-	packet[0x2E] = 0x46;
-	packet[0x2F] = 0x0E;
-	packet[0x35] = 0x01;
-
-	device.write(packet, 65);
 }
 
-function sendColorBurstThree(overrideColor) {
-	const packet = [];
+const NZXTdeviceLibrary = new deviceLibrary();
+const NZXT = new NZXT_Keyboard_Protocol();
 
-	for(let zone_idx = 0; zone_idx < 3; zone_idx++) {
-		const offset = zone_idx + 5;
-		const iX = vLedPositions[offset][0];
-		const iY = vLedPositions[offset][1];
-		let col;
-
-		if(overrideColor) {
-			col = hexToRgb(overrideColor);
-		} else if (LightingMode === "Forced") {
-			col = hexToRgb(forcedColor);
-		} else {
-			col = device.color(iX, iY);
-		}
-
-		packet[(zone_idx * 22) + 11] = col[0];
-		packet[(zone_idx * 22) + 12] = col[1];
-		packet[(zone_idx * 22) + 13] = col[2];
-	}
-
-	packet[0x00]   = 0x43;
-	packet[0x01]   = 0x3D;
-	packet[0x02]   = 0x01;
-	packet[0x03]   = 0x00;
-	packet[0x04]   = 0x00;
-
-	packet[0x05] = 0x32;
-	packet[0x06] = 0x04;
-	packet[0x07] = 0xB0;
-	packet[0x08] = 0x11;
-	packet[0x09] = 0x00;
-	packet[0x0A] = 0x00;
-	packet[0x0E] = 0x01;
-	packet[0x0F] = 0x00;
-	packet[0x10] = 0x40;
-
-	packet[0x18] = 0x40;
-	packet[0x19] = 0x00;
-	packet[0x1A] = 0x40;
-	packet[0x1B] = 0x08;
-	packet[0x1C] = 0x43;
-	packet[0x1D] = 0x08;
-	packet[0x1E] = 0x40;
-	packet[0x1F] = 0x00;
-	packet[0x20] = 0x00;
-	packet[0x24] = 0x01;
-	packet[0x25] = 0x00;
-	packet[0x26] = 0x30;
-	packet[0x27] = 0x00;
-	packet[0x28] = 0x40;
-	packet[0x29] = 0x00;
-	packet[0x2A] = 0x60;
-	packet[0x2B] = 0x00;
-	packet[0x2C] = 0x60;
-	packet[0x2D] = 0x00;
-	packet[0x2E] = 0x20;
-	packet[0x2F] = 0x00;
-	packet[0x30] = 0x34;
-
-	packet[54] = 0x00;
-	packet[58] = 0x01;
-
-	packet[60] = 0x0E;
-	packet[62] = 0x1C;
-
-	device.write(packet, 65);
-}
-
-function sendColorBurstFour(overrideColor) {
-	const packet = [];
-
-	for(let zone_idx = 0; zone_idx < 2; zone_idx++) {
-		const offset = zone_idx + 8;
-		const iX = vLedPositions[offset][0];
-		const iY = vLedPositions[offset][1];
-		let col;
-
-		if(overrideColor) {
-			col = hexToRgb(overrideColor);
-		} else if (LightingMode == "Forced") {
-			col = hexToRgb(forcedColor);
-		} else {
-			col = device.color(iX, iY);
-		}
-
-		packet[(zone_idx * 22) + 16] = col[0];
-		packet[(zone_idx * 22) + 17] = col[1];
-		packet[(zone_idx * 22) + 18] = col[2];
-	}
-
-	packet[0x00]   = 0x43;
-	packet[0x01]   = 0x26;
-	packet[0x02]   = 0x00;
-	packet[0x03]   = 0x1c;
-	packet[0x04]   = 0x00;
-
-	packet[0x05] = 0x0c;
-	packet[0x06] = 0x00;
-	packet[0x07] = 0x0c;
-	packet[0x08] = 0x00;
-	packet[0x09] = 0x0A;
-	packet[0x0A] = 0x00;
-	packet[0x0B] = 0x00;
-	packet[0x0C] = 0x00;
-	packet[0x0D] = 0x00;
-	packet[0x0E] = 0x00;
-	packet[0x0F] = 0x00;
-
-	packet[0x13] = 0x01;
-	packet[0x14] = 0x18;
-	packet[0x16] = 0x30;
-	packet[0x18] = 0x30;
-	packet[0x1A] = 0x30;
-	packet[0x1C] = 0x30;
-
-	device.write(packet, 65);
+function CompareArrays(array1, array2){
+	return array1.length === array2.length &&
+	array1.every(function(value, index) { return value === array2[index];});
 }
 
 function hexToRgb(hex) {
@@ -322,5 +195,5 @@ export function Validate(endpoint) {
 }
 
 export function ImageUrl() {
-	return "https://assets.signalrgb.com/devices/brands/nzxt/keyboards/function.png";
+	return "https://marketplace.signalrgb.com/devices/default/keyboards/full-size-keyboard-render.png";
 }
